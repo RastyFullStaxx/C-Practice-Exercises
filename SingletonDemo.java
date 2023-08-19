e478585fc0809d5ddc7fb522d2554f51d826050a
Rasty Espartero <gemrasty@gmail.com>
2023-08-19 23:13:42 +0800

    Add files via upload

---
+++ SingletonDemo.java
@@ -0,0 +1,22 @@
+class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static synchronized Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
+
+public class SingletonDemo {
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        System.out.println(s1 == s2); // Should print true
+    }
+}
