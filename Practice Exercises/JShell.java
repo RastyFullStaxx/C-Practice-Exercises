b8f78a7a66713784c84a1060ecd0ba67a3e7dd43
Rasty Espartero <gemrasty@gmail.com>
2023-09-09 23:27:30 +0800

    Create JShell.java

---
+++ Practice Exercises/JShell.java
@@ -0,0 +1,9 @@
+// This code is run interactively in the JShell tool
+
+String greeting = "Hello, JShell!";
+System.out.println(greeting);
+
+// Define custom functions interactively
+int square(int x) {
+    return x * x;
+}
