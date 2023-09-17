5dfe5c81c812b41e8e56a6ad74149e74fae4d884
Rasty Espartero <gemrasty@gmail.com>
2023-09-17 18:47:38 +0800

    Create Decimal to Binary Conversion.java

---
+++ Practice Exercises/Decimal to Binary Conversion.java
@@ -0,0 +1,8 @@
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        int decimal = 42;
+        String binaryString = Integer.toBinaryString(decimal);
+        System.out.println("Decimal: " + decimal);
+        System.out.println("Binary: " + binaryString);
+    }
+}
