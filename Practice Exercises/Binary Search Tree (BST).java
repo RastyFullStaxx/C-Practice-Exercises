a1182bda7f5de6f2ed29415f37e0035a88eb9f27
Rasty Espartero <gemrasty@gmail.com>
2023-09-17 18:50:41 +0800

    Create Binary Search Tree (BST).java

---
+++ Practice Exercises/Binary Search Tree (BST).java
@@ -0,0 +1,60 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class BinarySearchTree {
+    private TreeNode root;
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    public void insert(int val) {
+        root = insertRec(root, val);
+    }
+
+    private TreeNode insertRec(TreeNode root, int val) {
+        if (root == null) {
+            root = new TreeNode(val);
+            return root;
+        }
+
+        if (val < root.val) {
+            root.left = insertRec(root.left, val);
+        } else if (val > root.val) {
+            root.right = insertRec(root.right, val);
+        }
+
+        return root;
+    }
+
+    public void inOrderTraversal() {
+        inOrderTraversal(root);
+    }
+
+    private void inOrderTraversal(TreeNode root) {
+        if (root != null) {
+            inOrderTraversal(root.left);
+            System.out.print(root.val + " ");
+            inOrderTraversal(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(50);
+        bst.insert(30);
+        bst.insert(70);
+        bst.insert(20);
+        bst.insert(40);
+
+        System.out.print("In-order Traversal: ");
+        bst.inOrderTraversal();
+    }
+}
