9809692f2f5ff26a6c6ecb48b5dbde8c2eae2643
Rasty Espartero <gemrasty@gmail.com>
2023-09-19 23:28:07 +0800

    Create Binary Search Algorithm (Recursive).java

---
+++ Practice Exercises/Binary Search Algorithm (Recursive).java
@@ -0,0 +1,36 @@
+public class RecursiveBinarySearch {
+    public static void main(String[] args) {
+        int[] sortedArray = {1, 3, 5, 7, 9, 11, 13, 15};
+        int target = 7;
+
+        int result = binarySearch(sortedArray, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found.");
+        }
+    }
+
+    public static int binarySearch(int[] arr, int target) {
+        return binarySearchRecursive(arr, target, 0, arr.length - 1);
+    }
+
+    private static int binarySearchRecursive(int[] arr, int target, int left, int right) {
+        if (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            }
+
+            if (arr[mid] < target) {
+                return binarySearchRecursive(arr, target, mid + 1, right);
+            }
+
+            return binarySearchRecursive(arr, target, left, mid - 1);
+        }
+
+        return -1;
+    }
+}
