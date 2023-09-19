df6158fe8cd7e8112ed9bc73a158b67e6a0dc47a
Rasty Espartero <gemrasty@gmail.com>
2023-09-19 23:28:48 +0800

    Create Matrix Transposition.java

---
+++ Practice Exercises/Matrix Transposition.java
@@ -0,0 +1,27 @@
+public class MatrixTransposition {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] transposedMatrix = transposeMatrix(matrix);
+
+        for (int i = 0; i < transposedMatrix.length; i++) {
+            for (int j = 0; j < transposedMatrix[i].length; j++) {
+                System.out.print(transposedMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] transposeMatrix(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] result = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+}
