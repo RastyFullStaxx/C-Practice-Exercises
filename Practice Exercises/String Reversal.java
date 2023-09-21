a3d31121945bc0a111a47e980b46c939f776deab
Rasty Espartero <gemrasty@gmail.com>
2023-09-21 23:40:53 +0800

    Create String Reversal.java

---
+++ Practice Exercises/String Reversal.java
@@ -0,0 +1,24 @@
+public class StringReversal {
+    public static void main(String[] args) {
+        String input = "Hello, World!";
+        String reversed = reverseString(input);
+        System.out.println("Original: " + input);
+        System.out.println("Reversed: " + reversed);
+    }
+
+    public static String reverseString(String input) {
+        char[] charArray = input.toCharArray();
+        int left = 0;
+        int right = input.length() - 1;
+
+        while (left < right) {
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+            left++;
+            right--;
+        }
+
+        return new String(charArray);
+    }
+}
