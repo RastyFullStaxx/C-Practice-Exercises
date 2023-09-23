ae6a392aa20f82f49087b5fbf86885a94088d1b2
Rasty Espartero <gemrasty@gmail.com>
2023-09-23 22:23:13 +0800

    Create Binary Tree Traversal (Inorder, Preorder, Postorder).java

---
+++ Practice Exercises/Binary Tree Traversal (Inorder, Preorder, Postorder).java
@@ -0,0 +1,52 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class BinaryTreeTraversal {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        System.out.print("Inorder Traversal: ");
+        inorderTraversal(root);
+
+        System.out.print("\nPreorder Traversal: ");
+        preorderTraversal(root);
+
+        System.out.print("\nPostorder Traversal: ");
+        postorderTraversal(root);
+    }
+
+    public static void inorderTraversal(TreeNode root) {
+        if (root != null) {
+            inorderTraversal(root.left);
+            System.out.print(root.val + " ");
+            inorderTraversal(root.right);
+        }
+    }
+
+    public static void preorderTraversal(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.val + " ");
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
+        }
+    }
+
+    public static void postorderTraversal(TreeNode root) {
+        if (root != null) {
+            postorderTraversal(root.left);
+            postorderTraversal(root.right);
+            System.out.print(root.val + " ");
+        }
+    }
+}
