e63c20a2e4a0badd7ced819f37572a3b952692e5
Rasty Espartero <gemrasty@gmail.com>
2023-09-29 23:12:22 +0800

    Create Merge Two Sorted Arrays.java

---
+++ Practice Exercises/Merge Two Sorted Arrays.java
@@ -0,0 +1,37 @@
+public class MergeSortedArrays {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5, 7};
+        int[] arr2 = {2, 4, 6, 8};
+        int[] merged = mergeArrays(arr1, arr2);
+
+        System.out.print("Merged Array: ");
+        for (int num : merged) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] mergeArrays(int[] arr1, int[] arr2) {
+        int m = arr1.length;
+        int n = arr2.length;
+        int[] merged = new int[m + n];
+
+        int i = 0, j = 0, k = 0;
+        while (i < m && j < n) {
+            if (arr1[i] < arr2[j]) {
+                merged[k++] = arr1[i++];
+            } else {
+                merged[k++] = arr2[j++];
+            }
+        }
+
+        while (i < m) {
+            merged[k++] = arr1[i++];
+        }
+
+        while (j < n) {
+            merged[k++] = arr2[j++];
+        }
+
+        return merged;
+    }
+}
