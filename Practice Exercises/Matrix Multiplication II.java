233ddebdef0af14ec77963c5439adb1f895e1e6f
Rasty Espartero <gemrasty@gmail.com>
2023-10-05 23:22:20 +0800

    Create Matrix Multiplication II.java

---
+++ Practice Exercises/Matrix Multiplication II.java
@@ -0,0 +1,34 @@
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        int[][] matrixA = {{1, 2}, {3, 4}};
+        int[][] matrixB = {{5, 6}, {7, 8}};
+
+        int[][] result = multiplyMatrices(matrixA, matrixB);
+
+        System.out.println("Resultant Matrix:");
+        for (int[] row : result) {
+            for (int value : row) {
+                System.out.print(value + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[][] multiplyMatrices(int[][] A, int[][] B) {
+        int rowsA = A.length;
+        int colsA = A[0].length;
+        int colsB = B[0].length;
+
+        int[][] result = new int[rowsA][colsB];
+
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                for (int k = 0; k < colsA; k++) {
+                    result[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        return result;
+    }
+}
