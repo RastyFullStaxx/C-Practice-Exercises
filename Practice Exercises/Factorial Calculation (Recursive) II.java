1731ad25cdb6c9478888bcda20ada2748c074313
Rasty Espartero <gemrasty@gmail.com>
2023-10-08 23:16:45 +0800

    Create  Factorial Calculation (Recursive) II.java

---
+++ Practice Exercises/Factorial Calculation (Recursive) II.java
@@ -0,0 +1,15 @@
+public class FactorialRecursive {
+    public static void main(String[] args) {
+        int n = 5;
+        long factorial = calculateFactorial(n);
+
+        System.out.println("Factorial of " + n + " is " + factorial);
+    }
+
+    public static long calculateFactorial(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * calculateFactorial(n - 1);
+    }
+}
