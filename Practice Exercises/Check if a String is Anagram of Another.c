7a6e31029284b43c82ea283e99f7874db60ae7a3
Rasty Espartero <gemrasty@gmail.com>
2023-10-15 23:43:08 +0800

    Create Check if a String is Anagram of Another.c

---
+++ Practice Exercises/Check if a String is Anagram of Another.c
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class AnagramChecker {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+        boolean areAnagrams = areAnagrams(str1, str2);
+
+        System.out.println("'" + str1 + "' and '" + str2 + "' are anagrams? " + areAnagrams);
+    }
+
+    public static boolean areAnagrams(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        return Arrays.equals(charArray1, charArray2);
+    }
+}
