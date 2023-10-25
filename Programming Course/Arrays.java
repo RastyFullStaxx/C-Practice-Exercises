1af682dddb3edf0630b09eb6a6ab098c89d050c4
Rasty Espartero <gemrasty@gmail.com>
2023-10-25 23:38:31 +0800

    Add files via upload

---
+++ Programming Course/Arrays.java
@@ -0,0 +1,16 @@
+public class Arrays {
+    public static void main(String[] args){
+        
+        String[] classmates = new String[5];
+
+        classmates[0] = "jeyEm";
+        classmates[1] = "dabid";
+        classmates[2] = "marc";
+        classmates[3] = "marian";
+        classmates[4] = "jhanice";
+
+        for (int i=0; i<classmates.length; i++){
+            System.out.println(classmates[i]);
+        }
+    }
+}
