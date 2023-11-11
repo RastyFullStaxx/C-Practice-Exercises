8c123f556c3cd78d5f861c50ee6d4aa1c439aca6
Rasty Espartero <gemrasty@gmail.com>
2023-11-11 09:11:41 +0800

    Create AccessModifiers.java

---
+++ Programming Course/AccessModifiers.java
@@ -0,0 +1,7 @@
+//  Access Modifiers in Java
+//  public      --      accessible anywhere
+//  <default>   --      accessible from within the package
+//  private     --      accessible from within the class
+//  protected   --      
+
+//  Mutators (setters) and accessors (getters)
