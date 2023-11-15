9f7f700fd35e6924d62d8198202f6ab66695c7cc
Rasty Espartero <gemrasty@gmail.com>
2023-11-15 22:58:31 +0800

    Create Check for a Prime Number.java

---
+++ Practice Exercises/Check for a Prime Number.java
@@ -0,0 +1,22 @@
+public class PrimeNumberChecker {
+    public static void main(String[] args) {
+        int num = 29;
+        boolean isPrime = isPrimeNumber(num);
+
+        System.out.println(num + " is a prime number? " + isPrime);
+    }
+
+    public static boolean isPrimeNumber(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
