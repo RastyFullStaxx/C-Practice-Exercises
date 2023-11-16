abadc4f966f9c342360f1d4d7ba6b951d1facff7
Rasty Espartero <gemrasty@gmail.com>
2023-11-17 01:05:14 +0800

    Add files via upload

---
+++ Programming Course/Pizza.java
@@ -0,0 +1,24 @@
+package ObjectsForBroCourse;
+
+public class Pizza {
+    
+    public String bread;
+    public String sauce;
+    public String cheese;
+    public String topping;
+
+    public Pizza(String bread, String sauce, String cheese, String topping){
+        this.bread = bread;
+        this.sauce = sauce;
+        this.cheese = cheese;
+        this.topping = topping;
+    }
+
+    public Pizza(String bread, String sauce, String topping){
+        this.bread = bread;
+        this.sauce = sauce;
+        this.topping = topping;
+    }
+
+
+}
