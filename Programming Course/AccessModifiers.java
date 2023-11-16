02b267effc47b5133b4e8b74bcc0a7b4583737d4
Rasty Espartero <gemrasty@gmail.com>
2023-11-17 00:36:29 +0800

    Delete Programming Course/AccessModifiers.java

--- Programming Course/AccessModifiers.java
+++
@@ -1,7 +0,0 @@
-//  Access Modifiers in Java
-//  public      --      accessible anywhere
-//  <default>   --      accessible from within the package
-//  private     --      accessible from within the class
-//  protected   --      
-
-//  Mutators (setters) and accessors (getters)
