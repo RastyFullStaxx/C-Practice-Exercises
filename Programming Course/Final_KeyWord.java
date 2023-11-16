015dd15d64acd32f613fc9e988b8cc480b74bdb0
Rasty Espartero <gemrasty@gmail.com>
2023-11-16 23:04:10 +0800

    Add files via upload

---
+++ Programming Course/Final_KeyWord.java
@@ -0,0 +1,11 @@
+public class Final_KeyWord {
+    public static void main(String[] args) {
+        
+        // Final Keyword    =   anything that is declared final cannot be changed or updated in the program
+
+            final double PI = 3.14159;          // a common practice is when declaring a variable as final is to name it in all caps
+
+            System.out.println(PI);
+
+    }
+}
