9263129f97790b852c94de5281a9b6856f4db259
Rasty Espartero <gemrasty@gmail.com>
2023-11-17 01:03:39 +0800

    Add files via upload

---
+++ Programming Course/packages/AccessMClass.java
@@ -0,0 +1,9 @@
+package ObjectsForBroCourse;
+
+public class AccessMClass {
+    
+    public      String publicMessage = "This is the public message";
+    protected   String protectedMessage = "This is the protected message";
+                String defaultMessage = "This is the default message";
+
+}
