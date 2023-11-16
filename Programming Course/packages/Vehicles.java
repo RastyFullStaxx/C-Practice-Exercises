a4fc6758196ac3188fd738ed096a369401f02c2d
Rasty Espartero <gemrasty@gmail.com>
2023-11-17 01:05:30 +0800

    Add files via upload

---
+++ Programming Course/packages/Vehicles.java
@@ -0,0 +1,15 @@
+package ObjectsForBroCourse;
+
+public class Vehicles {
+    
+    public double speed;
+
+    public void go() {
+        System.out.println("This vehicle accelerates...");
+    }
+
+    public void stop() {
+        System.out.println("This vehicle hit the breaks and stopped..");
+    }
+
+}
