9f14fddb50bf0ce8e75493e0d69c8c79128715c1
Rasty Espartero <gemrasty@gmail.com>
2023-11-17 01:05:00 +0800

    Add files via upload

---
+++ Programming Course/packages/person2.java
@@ -0,0 +1,18 @@
+package ObjectsForBroCourse;
+
+public class person2 {
+   
+    public String name;
+    public int age;
+
+    public person2(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return this.name + "\n" + this.age + "\n";
+    } 
+
+}
+ 
