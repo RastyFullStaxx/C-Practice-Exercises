f8604c45b3360ca40d1a118ed3e93d273650e9dd
Rasty Espartero <gemrasty@gmail.com>
2023-11-19 23:30:56 +0800

    Create SudokuValidator.java

---
+++ Practice Exercises/SudokuValidator.java
@@ -0,0 +1,26 @@
+public class SudokuValidator {
+    public static void main(String[] args) {
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        boolean isValid = isValidSudoku(board);
+
+        if (isValid) {
+            System.out.println("The Sudoku board is valid.");
+        } else {
+            System.out.println("The Sudoku board is not valid.");
+        }
+    }
+
+    public static boolean isValidSudoku(char[][] board) {
+        int[][] rows = new int[9][9];
+        int
