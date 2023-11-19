406fb2f6580ceb037efa733b07603a4cdd9acc45
Rasty Espartero <gemrasty@gmail.com>
2023-11-19 23:31:26 +0800

    Update SudokuValidator.java

--- Practice Exercises/SudokuValidator.java
+++ Practice Exercises/SudokuValidator.java
@@ -23,4 +23,26 @@ public class SudokuValidator {
 
     public static boolean isValidSudoku(char[][] board) {
         int[][] rows = new int[9][9];
-        int
+        int[][] cols = new int[9][9];
+        int[][] boxes = new int[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    int boxIndex = (i / 3) * 3 + j / 3;
+
+                    if (rows[i][num] == 1 || cols[j][num] == 1 || boxes[boxIndex][num] == 1) {
+                        return false;
+                    }
+
+                    rows[i][num] = 1;
+                    cols[j][num] = 1;
+                    boxes[boxIndex][num] = 1;
+                }
+            }
+        }
+
+        return true;
+    }
+}
