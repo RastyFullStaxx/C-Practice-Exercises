b5b87152433cf952a4a20bffc83250df037aab29
Rasty Espartero <gemrasty@gmail.com>
2023-12-05 22:13:13 +0800

    Create TopKFrequentElements.java

---
+++ Practice Exercises/Queues/TopKFrequentElements.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class TopKFrequentElements {
+    public static int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> frequencyMap.get(a) - frequencyMap.get(b));
+
+        for (int num : frequencyMap.keySet()) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        int[] result = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = minHeap.poll();
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int k = 2;
+        int[] result = topKFrequent(nums, k);
+        System.out.println("Top K frequent elements: " + Arrays.toString(result));
+    }
+}
