f00a79bb1a22bd452ec3b633914f32a9394c52ad
Rasty Espartero <gemrasty@gmail.com>
2023-12-06 23:49:26 +0800

    Create BTreeMaxKey.java

---
+++ Practice Exercises/B-Tree/BTreeMaxKey.java
@@ -0,0 +1,18 @@
+public class BTreeMaxKey {
+    public static void main(String[] args) {
+        BTreeNode root = buildSampleBTree();
+        int maxKey = findMaxKey(root);
+
+        System.out.println("Maximum key in the B-Tree: " + maxKey);
+    }
+
+    public static int findMaxKey(BTreeNode root) {
+        // Implement B-tree maximum key finding algorithm
+        return 0;
+    }
+
+    public static BTreeNode buildSampleBTree() {
+        // Helper method to build a sample B-tree for testing
+        return null;
+    }
+}
