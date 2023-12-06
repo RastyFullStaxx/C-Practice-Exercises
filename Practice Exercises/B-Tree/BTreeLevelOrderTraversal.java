5a8311c85280a5d0ac1369d58cb8b1d002c6c90a
Rasty Espartero <gemrasty@gmail.com>
2023-12-06 23:49:49 +0800

    Create BTreeLevelOrderTraversal.java

---
+++ Practice Exercises/B-Tree/BTreeLevelOrderTraversal.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BTreeLevelOrderTraversal {
+    public static void main(String[] args) {
+        BTreeNode root = buildSampleBTree();
+
+        System.out.println("Level order traversal of the B-Tree:");
+        levelOrderTraversal(root);
+    }
+
+    public static void levelOrderTraversal(BTreeNode root) {
+        // Implement B-tree level order traversal
+    }
+
+    public static BTreeNode buildSampleBTree() {
+        // Helper method to build a sample B-tree for testing
+        return null;
+    }
+}
