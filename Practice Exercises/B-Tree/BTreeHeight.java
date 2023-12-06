22b0cc371294ab1f1aafc4ee622901d74b5ea4e5
Rasty Espartero <gemrasty@gmail.com>
2023-12-06 23:50:02 +0800

    Create BTreeHeight.java

---
+++ Practice Exercises/B-Tree/BTreeHeight.java
@@ -0,0 +1,18 @@
+public class BTreeHeight {
+    public static void main(String[] args) {
+        BTreeNode root = buildSampleBTree();
+        int height = calculateHeight(root);
+
+        System.out.println("Height of the B-Tree: " + height);
+    }
+
+    public static int calculateHeight(BTreeNode root) {
+        // Implement B-tree height calculation algorithm
+        return 0;
+    }
+
+    public static BTreeNode buildSampleBTree() {
+        // Helper method to build a sample B-tree for testing
+        return null;
+    }
+}
