32edb9050e534a66d5e4a752f445a772bc019529
Rasty Espartero <gemrasty@gmail.com>
2023-12-06 23:49:38 +0800

    Create BTreeInorderTraversal.java

---
+++ Practice Exercises/B-Tree/BTreeInorderTraversal.java
@@ -0,0 +1,17 @@
+public class BTreeInorderTraversal {
+    public static void main(String[] args) {
+        BTreeNode root = buildSampleBTree();
+
+        System.out.println("Inorder traversal of the B-Tree:");
+        inorderTraversal(root);
+    }
+
+    public static void inorderTraversal(BTreeNode root) {
+        // Implement B-tree inorder traversal
+    }
+
+    public static BTreeNode buildSampleBTree() {
+        // Helper method to build a sample B-tree for testing
+        return null;
+    }
+}
