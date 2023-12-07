e9f91202e90e71d009f80cf9cd746cf8cecfd9de
Rasty Espartero <gemrasty@gmail.com>
2023-12-07 23:42:17 +0800

    Create BTreePredecessorSuccessor.java

---
+++ Practice Exercises/B-Tree/BTreePredecessorSuccessor.java
@@ -0,0 +1,17 @@
+public class BTreePredecessorSuccessor {
+    public static void main(String[] args) {
+        BTreeNode root = buildSampleBTree();
+        int key = 15;
+
+        findPredecessorSuccessor(root, key);
+    }
+
+    public static void findPredecessorSuccessor(BTreeNode root, int key) {
+        // Implement B-tree predecessor and successor finding algorithm
+    }
+
+    public static BTreeNode buildSampleBTree() {
+        // Helper method to build a sample B-tree for testing
+        return null;
+    }
+}
