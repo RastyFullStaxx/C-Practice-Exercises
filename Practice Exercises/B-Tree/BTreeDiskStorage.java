9684ab91bd5ee83c7a55dd5d3a9966fc6d80d973
Rasty Espartero <gemrasty@gmail.com>
2023-12-07 23:43:03 +0800

    Create BTreeDiskStorage.java

---
+++ Practice Exercises/B-Tree/BTreeDiskStorage.java
@@ -0,0 +1,7 @@
+public class BTreeDiskStorage {
+    public static void main(String[] args) {
+        // Implement B-tree disk-based storage
+    }
+
+    // Additional methods for disk-based B-tree storage
+}
