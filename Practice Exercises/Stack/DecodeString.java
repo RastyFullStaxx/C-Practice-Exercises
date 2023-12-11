3401f82c12bc2a772597fb7dc0b9c9f844dd53c6
Rasty Espartero <gemrasty@gmail.com>
2023-12-11 23:39:17 +0800

    Create DecodeString.java

---
+++ Practice Exercises/Stack/DecodeString.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class DecodeString {
+    public static void main(String[] args) {
+        String s = "3[a]2[bc]";
+
+        String decodedString = decodeString(s);
+
+        System.out.println("Decoded String: " + decodedString);
+    }
+
+    public static String decodeString(String s) {
+        // Implement a function to decode a given encoded string
+        return "";
+    }
+}
