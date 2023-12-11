0f76e53a95034aaba41273f4e570d7655389ec82
Rasty Espartero <gemrasty@gmail.com>
2023-12-11 23:39:37 +0800

    Create EnhancedAsteroidCollision.java

---
+++ Practice Exercises/Stack/EnhancedAsteroidCollision.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class EnhancedAsteroidCollision {
+    public static void main(String[] args) {
+        int[] asteroids = {5, 10, -5, 7, -7, -8};
+
+        int[] result = enhancedAsteroidCollision(asteroids);
+
+        System.out.println("Result after asteroid collision: " + Arrays.toString(result));
+    }
+
+    public static int[] enhancedAsteroidCollision(int[] asteroids) {
+        // Implement a function to simulate asteroid collision and return the remaining asteroids
+        return new int[0];
+    }
+}
