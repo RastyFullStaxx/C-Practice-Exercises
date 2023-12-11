9caa26241083c0d7559d71e1a2c1ea213445c19c
Rasty Espartero <gemrasty@gmail.com>
2023-12-11 23:39:08 +0800

    Create BaseballGame.java

---
+++ Practice Exercises/Stack/BaseballGame.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class BaseballGame {
+    public static void main(String[] args) {
+        String[] operations = {"5", "2", "C", "D", "+"};
+
+        int totalScore = calPoints(operations);
+
+        System.out.println("Total score: " + totalScore);
+    }
+
+    public static int calPoints(String[] ops) {
+        // Implement a function to calculate the total score in a baseball game
+        return 0;
+    }
+}
