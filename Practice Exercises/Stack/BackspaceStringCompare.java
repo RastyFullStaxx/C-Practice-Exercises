07330d4e339a8853830d20d83e95855e4e5a0a55
Rasty Espartero <gemrasty@gmail.com>
2023-12-11 23:38:48 +0800

    Create BackspaceStringCompare.java

---
+++ Practice Exercises/Stack/BackspaceStringCompare.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+public class BackspaceStringCompare {
+    public static void main(String[] args) {
+        String s1 = "ab#c";
+        String t1 = "ad#c";
+        String s2 = "ab##";
+        String t2 = "c#d#";
+
+        System.out.println("Are strings 1 equal? " + backspaceCompare(s1, t1));
+        System.out.println("Are strings 2 equal? " + backspaceCompare(s2, t2));
+    }
+
+    public static boolean backspaceCompare(String s, String t) {
+        // Implement a function to check if two strings are equal after processing backspace characters
+        return false;
+    }
+}
