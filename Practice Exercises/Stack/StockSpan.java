60c8daa3a3b21cb1da85701658dc2f0d92aaab5e
Rasty Espartero <gemrasty@gmail.com>
2023-12-11 23:38:16 +0800

    Create StockSpan.java

---
+++ Practice Exercises/Stack/StockSpan.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class StockSpan {
+    public static void main(String[] args) {
+        int[] prices = {100, 80, 60, 70, 60, 75, 85};
+
+        int[] span = calculateStockSpan(prices);
+
+        System.out.println("Stock Span for each day: " + Arrays.toString(span));
+    }
+
+    public static int[] calculateStockSpan(int[] prices) {
+        // Implement a function to calculate the stock span for each day
+        return new int[0];
+    }
+}
