c7a9d1dac218f1ab9c7d03c28ea9c7366d38a69b
Rasty Espartero <gemrasty@gmail.com>
2023-12-12 23:35:46 +0800

    Create RemoveDuplicateLetters.java

---
+++ Practice Exercises/Queues/RemoveDuplicateLetters.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class RemoveDuplicateLetters {
+    public static void main(String[] args) {
+        String s = "bcabc";
+
+        String result = removeDuplicateLetters(s);
+
+        System.out.println("Result after removing duplicate letters: " + result);
+    }
+
+    public static String removeDuplicateLetters(String s) {
+        // Implement a function to remove duplicate letters and return the smallest lexicographical result
+        return "";
+    }
+}
