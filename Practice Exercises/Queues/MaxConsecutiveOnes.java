c6b414e33b5180edad1762562536a240fb188fe1
Rasty Espartero <gemrasty@gmail.com>
2023-12-12 23:36:41 +0800

    Create MaxConsecutiveOnes.java

---
+++ Practice Exercises/Queues/MaxConsecutiveOnes.java
@@ -0,0 +1,14 @@
+public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+        int[] nums = {1, 0, 1, 1, 0, 1};
+
+        int maxConsecutiveOnes = findMaxConsecutiveOnes(nums);
+
+        System.out.println("Max Consecutive Ones after flipping one zero: " + maxConsecutiveOnes);
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        // Implement a function to find the maximum number of consecutive ones after flipping at most one zero
+        return 0;
+    }
+}
