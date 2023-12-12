5b78b311bf8afa1a1b3af220e2eb184595db60e5
Rasty Espartero <gemrasty@gmail.com>
2023-12-12 23:34:21 +0800

    Create SimplifyExpression.java

---
+++ Practice Exercises/Queues/SimplifyExpression.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class SimplifyExpression {
+    public static void main(String[] args) {
+        String expression = "3 + 5 * ( 2 - 8 ) / 4";
+
+        int result = simplifyExpression(expression);
+
+        System.out.println("Result after simplification: " + result);
+    }
+
+    public static int simplifyExpression(String expression) {
+        // Implement a function to simplify a mathematical expression using a stack
+        return 0;
+    }
+}
