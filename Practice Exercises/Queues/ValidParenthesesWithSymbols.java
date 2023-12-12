79cc3604c096ca41a5e9018c967682409fb188f7
Rasty Espartero <gemrasty@gmail.com>
2023-12-12 23:34:58 +0800

    Create ValidParenthesesWithSymbols.java

---
+++ Practice Exercises/Queues/ValidParenthesesWithSymbols.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class ValidParenthesesWithSymbols {
+    public static void main(String[] args) {
+        String expression1 = "{[(a+b)]}";
+        String expression2 = "{[a+b)}";
+
+        System.out.println("Expression 1 is valid: " + isValid(expression1));
+        System.out.println("Expression 2 is valid: " + isValid(expression2));
+    }
+
+    public static boolean isValid(String expression) {
+        // Implement a function to check if the given expression with custom symbols has valid parentheses
+        return false;
+    }
+}
