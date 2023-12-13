eed261a143096dab86b86ef57c50aaaf2e80ed7f
Rasty Espartero <gemrasty@gmail.com>
2023-12-13 23:56:29 +0800

    Create StringBubbleSort.java

---
+++ Practice Exercises/Bubble Sort/StringBubbleSort.java
@@ -0,0 +1,13 @@
+public class StringBubbleSort {
+    public static void main(String[] args) {
+        String[] array = {"apple", "banana", "orange", "kiwi", "grape"};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        bubbleSortStrings(array);
+        System.out.println("Sorted Array: " + Arrays.toString(array));
+    }
+
+    public static void bubbleSortStrings(String[] arr) {
+        // Implement the Bubble Sort algorithm to sort an array of strings
+    }
+}
