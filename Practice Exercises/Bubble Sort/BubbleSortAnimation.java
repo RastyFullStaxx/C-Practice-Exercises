7d043f970dde61b666143a7dafd0b9ee76387d8c
Rasty Espartero <gemrasty@gmail.com>
2023-12-13 23:56:16 +0800

    Create BubbleSortAnimation.java

---
+++ Practice Exercises/Bubble Sort/BubbleSortAnimation.java
@@ -0,0 +1,13 @@
+public class BubbleSortAnimation {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        animateBubbleSort(array);
+        System.out.println("Sorted Array: " + Arrays.toString(array));
+    }
+
+    public static void animateBubbleSort(int[] arr) {
+        // Implement the Bubble Sort algorithm and print the array at each step for visualization
+    }
+}
