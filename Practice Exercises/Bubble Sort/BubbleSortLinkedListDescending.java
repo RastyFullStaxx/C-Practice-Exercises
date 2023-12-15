ac1bbbf05ebb664337963d5c75c6e5e9ebc089ab
Rasty Espartero <gemrasty@gmail.com>
2023-12-15 22:48:45 +0800

    Create BubbleSortLinkedListDescending.java

---
+++ Practice Exercises/Bubble Sort/BubbleSortLinkedListDescending.java
@@ -0,0 +1,36 @@
+public class BubbleSortLinkedListDescending {
+    public static void main(String[] args) {
+        ListNode head = createLinkedList();
+
+        System.out.println("Original Linked List: ");
+        printLinkedList(head);
+
+        head = bubbleSortLinkedListDescending(head);
+
+        System.out.println("Sorted Linked List in Descending Order: ");
+        printLinkedList(head);
+    }
+
+    public static ListNode bubbleSortLinkedListDescending(ListNode head) {
+        // Implement the Bubble Sort algorithm to sort a linked list in descending order
+        return null;
+    }
+
+    public static ListNode createLinkedList() {
+        // Helper method to create a sample linked list
+        return null;
+    }
+
+    public static void printLinkedList(ListNode head) {
+        // Helper method to print the elements of a linked list
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
