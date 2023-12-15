784cd244036c84bb475225e1ae5168f1418db3b3
Rasty Espartero <gemrasty@gmail.com>
2023-12-15 22:48:14 +0800

    Create EarlyTerminationBubbleSort.java

---
+++ Practice Exercises/Bubble Sort/EarlyTerminationBubbleSort.java
@@ -0,0 +1,13 @@
+public class EarlyTerminationBubbleSort {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        earlyTerminationBubbleSort(array);
+        System.out.println("Sorted Array with Early Termination: " + Arrays.toString(array));
+    }
+
+    public static void earlyTerminationBubbleSort(int[] arr) {
+        // Implement the Bubble Sort algorithm with early termination if no swaps are performed
+    }
+}
