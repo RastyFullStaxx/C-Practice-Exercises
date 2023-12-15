92f839f85804b19fcd2a2283952cbb687ec27058
Rasty Espartero <gemrasty@gmail.com>
2023-12-15 22:47:10 +0800

    Create OddEvenBubbleSort.java

---
+++ Practice Exercises/Bubble Sort/OddEvenBubbleSort.java
@@ -0,0 +1,13 @@
+public class OddEvenBubbleSort {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        oddEvenBubbleSort(array);
+        System.out.println("Sorted Array with Odd-Even Sorting: " + Arrays.toString(array));
+    }
+
+    public static void oddEvenBubbleSort(int[] arr) {
+        // Implement the Bubble Sort algorithm with odd and even index sorting
+    }
+}
