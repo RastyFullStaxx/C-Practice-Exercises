f0e4b2b4c874b2f0423df303a5a1febcbfedbe79
Rasty Espartero <gemrasty@gmail.com>
2023-12-15 22:47:52 +0800

    Create StableBubbleSort.java

---
+++ Practice Exercises/Bubble Sort/StableBubbleSort.java
@@ -0,0 +1,13 @@
+public class StableBubbleSort {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        stableBubbleSort(array);
+        System.out.println("Sorted Array with Stable Bubble Sort: " + Arrays.toString(array));
+    }
+
+    public static void stableBubbleSort(int[] arr) {
+        // Implement the Bubble Sort algorithm with stability
+    }
+}
