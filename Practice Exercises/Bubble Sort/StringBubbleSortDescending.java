80bd981331fa87d053a0716316f8559898dc92da
Rasty Espartero <gemrasty@gmail.com>
2023-12-15 22:47:40 +0800

    Create StringBubbleSortDescending.java

---
+++ Practice Exercises/Bubble Sort/StringBubbleSortDescending.java
@@ -0,0 +1,13 @@
+public class StringBubbleSortDescending {
+    public static void main(String[] args) {
+        String[] array = {"apple", "banana", "orange", "kiwi", "grape"};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        bubbleSortStringsDescending(array);
+        System.out.println("Sorted Array of Strings in Descending Order: " + Arrays.toString(array));
+    }
+
+    public static void bubbleSortStringsDescending(String[] arr) {
+        // Implement the Bubble Sort algorithm to sort an array of strings in descending order
+    }
+}
