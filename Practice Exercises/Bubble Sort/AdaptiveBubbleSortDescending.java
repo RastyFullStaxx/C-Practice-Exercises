0d63a91981f1a4ef66f10e1aa9755062788d3e65
Rasty Espartero <gemrasty@gmail.com>
2023-12-15 22:47:20 +0800

    Create AdaptiveBubbleSortDescending.java

---
+++ Practice Exercises/Bubble Sort/AdaptiveBubbleSortDescending.java
@@ -0,0 +1,13 @@
+public class AdaptiveBubbleSortDescending {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        adaptiveBubbleSortDescending(array);
+        System.out.println("Sorted Array with Adaptive Optimization (Descending): " + Arrays.toString(array));
+    }
+
+    public static void adaptiveBubbleSortDescending(int[] arr) {
+        // Implement the Bubble Sort algorithm with adaptive optimization for descending order
+    }
+}
