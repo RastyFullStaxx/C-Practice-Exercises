8f316b49d720036f0b69506d721103e8e9fddd4e
Rasty Espartero <gemrasty@gmail.com>
2023-12-15 22:48:24 +0800

    Create NearlySortedBubbleSortDescending.java

---
+++ Practice Exercises/Bubble Sort/NearlySortedBubbleSortDescending.java
@@ -0,0 +1,13 @@
+public class NearlySortedBubbleSortDescending {
+    public static void main(String[] args) {
+        int[] array = {2, 1, 3, 4, 6, 5};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        bubbleSortForNearlySortedDescending(array, 2);
+        System.out.println("Sorted Array for Nearly Sorted Input in Descending Order: " + Arrays.toString(array));
+    }
+
+    public static void bubbleSortForNearlySortedDescending(int[] arr, int k) {
+        // Implement the Bubble Sort algorithm for nearly sorted arrays with a given value of k in descending order
+    }
+}
