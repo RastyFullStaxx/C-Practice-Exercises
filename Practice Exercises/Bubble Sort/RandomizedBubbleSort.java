f4bfe3cfd559d073da1a20133c7fe4d5fdc49133
Rasty Espartero <gemrasty@gmail.com>
2023-12-15 22:48:34 +0800

    Create RandomizedBubbleSort.java

---
+++ Practice Exercises/Bubble Sort/RandomizedBubbleSort.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class RandomizedBubbleSort {
+    public static void main(String[] args) {
+        int[] array = generateRandomArray(10);
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        bubbleSort(array);
+        System.out.println("Sorted Array with Randomized Input: " + Arrays.toString(array));
+    }
+
+    public static int[] generateRandomArray(int size) {
+        // Helper method to generate a random array of given size
+        return null;
+    }
+
+    public static void bubbleSort(int[] arr) {
+        // Implement the Bubble Sort algorithm for a randomly generated input
+    }
+}
