756ddc2e7324038b2074780c85eec64f75573d19
Rasty Espartero <gemrasty@gmail.com>
2023-12-16 23:45:49 +0800

    Create CircularBubbleSort.java

---
+++ Practice Exercises/Bubble Sort/CircularBubbleSort.java
@@ -0,0 +1,16 @@
+
+**Exercise 168: Circular Bubble Sort**
+```java
+public class CircularBubbleSort {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        circularBubbleSort(array);
+        System.out.println("Sorted Array with Circular Bubble Sort: " + Arrays.toString(array));
+    }
+
+    public static void circularBubbleSort(int[] arr) {
+        // Implement the Circular Bubble Sort algorithm
+    }
+}
