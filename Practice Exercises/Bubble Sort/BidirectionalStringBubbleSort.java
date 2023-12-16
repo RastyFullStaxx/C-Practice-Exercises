898b24f7e8b6b442f67628fb8cef167e7dab577d
Rasty Espartero <gemrasty@gmail.com>
2023-12-16 23:45:18 +0800

    Create BidirectionalStringBubbleSort.java

---
+++ Practice Exercises/Bubble Sort/BidirectionalStringBubbleSort.java
@@ -0,0 +1,13 @@
+public class BidirectionalStringBubbleSort {
+    public static void main(String[] args) {
+        String[] array = {"apple", "banana", "orange", "kiwi", "grape"};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        bidirectionalStringBubbleSort(array);
+        System.out.println("Sorted Array with Bidirectional Bubble Sort: " + Arrays.toString(array));
+    }
+
+    public static void bidirectionalStringBubbleSort(String[] arr) {
+        // Implement the Bidirectional Bubble Sort algorithm for an array of strings
+    }
+}
