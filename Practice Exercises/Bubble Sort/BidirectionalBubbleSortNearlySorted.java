8784fbf934e26e3550d59248e997ddc32a4ad0b5
Rasty Espartero <gemrasty@gmail.com>
2023-12-16 23:46:08 +0800

    Create BidirectionalBubbleSortNearlySorted.java

---
+++ Practice Exercises/Bubble Sort/BidirectionalBubbleSortNearlySorted.java
@@ -0,0 +1,13 @@
+public class BidirectionalBubbleSortNearlySorted {
+    public static void main(String[] args) {
+        int[] array = {2, 1, 3, 4, 6, 5};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        bidirectionalBubbleSortForNearlySorted(array, 2);
+        System.out.println("Sorted Array for Nearly Sorted Input: " + Arrays.toString(array));
+    }
+
+    public static void bidirectionalBubbleSortForNearlySorted(int[] arr, int k) {
+        // Implement the Bidirectional Bubble Sort algorithm for nearly sorted arrays with a given value of k
+    }
+}
