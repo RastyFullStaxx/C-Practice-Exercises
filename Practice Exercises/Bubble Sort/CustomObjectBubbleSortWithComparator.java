778813c2257340687674e5af84191f585fb19609
Rasty Espartero <gemrasty@gmail.com>
2023-12-16 23:45:59 +0800

    Create CustomObjectBubbleSortWithComparator.java

---
+++ Practice Exercises/Bubble Sort/CustomObjectBubbleSortWithComparator.java
@@ -0,0 +1,32 @@
+import java.util.Comparator;
+
+public class CustomObjectBubbleSortWithComparator {
+    public static void main(String[] args) {
+        Person[] people = {
+                new Person("Alice", 25),
+                new Person("Bob", 20),
+                new Person("Charlie", 30),
+                new Person("David", 22)
+        };
+
+        System.out.println("Original Array: " + Arrays.toString(people));
+        bubbleSortCustomObjectsWithComparator(people, Comparator.comparing(Person::getAge));
+        System.out.println("Sorted Array by Age: " + Arrays.toString(people));
+    }
+
+    public static void bubbleSortCustomObjectsWithComparator(Object[] arr, Comparator<Object> comparator) {
+        // Implement the Bubble Sort algorithm using a comparator for sorting custom objects
+    }
+}
+
+class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Implement getters and toString method
+}
