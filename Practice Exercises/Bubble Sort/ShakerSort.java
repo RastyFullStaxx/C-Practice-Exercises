e9e1be1add846fa3853877bfb92b2087cc57951f
Rasty Espartero <gemrasty@gmail.com>
2023-12-16 23:46:29 +0800

    Create ShakerSort.java

---
+++ Practice Exercises/Bubble Sort/ShakerSort.java
@@ -0,0 +1,13 @@
+public class ShakerSort {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        shakerSort(array);
+        System.out.println("Sorted Array with Shaker Sort: " + Arrays.toString(array));
+    }
+
+    public static void shakerSort(int[] arr) {
+        // Implement the Shaker Sort (Cocktail Sort) algorithm
+    }
+}
