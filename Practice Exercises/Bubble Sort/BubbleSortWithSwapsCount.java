3a290e6ef84ce3e68b025d506a886a968334ba13
Rasty Espartero <gemrasty@gmail.com>
2023-12-16 23:45:28 +0800

    Create BubbleSortWithSwapsCount.java

---
+++ Practice Exercises/Bubble Sort/BubbleSortWithSwapsCount.java
@@ -0,0 +1,6 @@
+public class BubbleSortWithSwapsCount {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        int swaps
