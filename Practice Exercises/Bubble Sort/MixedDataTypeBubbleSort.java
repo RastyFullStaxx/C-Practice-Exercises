c0df6ecad4336569dbd3211e833f2433f2e2924d
Rasty Espartero <gemrasty@gmail.com>
2023-12-17 23:51:35 +0800

    Create MixedDataTypeBubbleSort.java

---
+++ Practice Exercises/Bubble Sort/MixedDataTypeBubbleSort.java
@@ -0,0 +1,13 @@
+public class MixedDataTypeBubbleSort {
+    public static void main(String[] args) {
+        Object[] mixedArray = {"apple", 34, "orange", 12, 22, "banana", 90};
+
+        System.out.println("Original Array: " + Arrays.toString(mixedArray));
+        bubbleSortMixedDataType(mixedArray);
+        System.out.println("Sorted Array with Multiple Data Types: " + Arrays.toString(mixedArray));
+    }
+
+    public static void bubbleSortMixedDataType(Object[] arr) {
+        // Implement the Bubble Sort algorithm for an array with multiple data types
+    }
+}
