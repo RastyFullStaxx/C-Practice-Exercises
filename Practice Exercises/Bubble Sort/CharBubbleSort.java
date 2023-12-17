9508aa7963c780f8348f1f8ca2012055bef94427
Rasty Espartero <gemrasty@gmail.com>
2023-12-17 23:53:45 +0800

    Create CharBubbleSort.java

---
+++ Practice Exercises/Bubble Sort/CharBubbleSort.java
@@ -0,0 +1,15 @@
+public class CharBubbleSort {
+    public static void main(String[] args) {
+        String input = "rasty";
+
+        System.out.println("Original String: " + input);
+        String sortedString = bubbleSortChars(input);
+        System.out.println("Sorted String: " + sortedString);
+    }
+
+    public static String bubbleSortChars(String str) {
+        // Implement the Bubble Sort algorithm to sort characters in a string
+        // Convert the string to a character array and apply the Bubble Sort algorithm
+        return null;
+    }
+}
