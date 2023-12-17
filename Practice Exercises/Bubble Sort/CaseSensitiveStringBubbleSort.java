2cdc098b72c5dd73d1ce12f97ae162226c8670b9
Rasty Espartero <gemrasty@gmail.com>
2023-12-17 23:52:26 +0800

    Create CaseSensitiveStringBubbleSort.java

---
+++ Practice Exercises/Bubble Sort/CaseSensitiveStringBubbleSort.java
@@ -0,0 +1,13 @@
+public class CaseSensitiveStringBubbleSort {
+    public static void main(String[] args) {
+        String[] array = {"apple", "Banana", "orange", "Kiwi", "Grape"};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        bubbleSortCaseSensitiveStrings(array);
+        System.out.println("Sorted Array with Case Sensitivity: " + Arrays.toString(array));
+    }
+
+    public static void bubbleSortCaseSensitiveStrings(String[] arr) {
+        // Implement the Bubble Sort algorithm for an array of strings with case sensitivity
+    }
+}
