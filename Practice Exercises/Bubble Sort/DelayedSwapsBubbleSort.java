c0e66da783986d4e4188174846649ff4020e5eeb
Rasty Espartero <gemrasty@gmail.com>
2023-12-17 23:52:16 +0800

    Create DelayedSwapsBubbleSort.java

---
+++ Practice Exercises/Bubble Sort/DelayedSwapsBubbleSort.java
@@ -0,0 +1,13 @@
+public class DelayedSwapsBubbleSort {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        delayedSwapsBubbleSort(array);
+        System.out.println("Sorted Array with Delayed Swaps: " + Arrays.toString(array));
+    }
+
+    public static void delayedSwapsBubbleSort(int[] arr) {
+        // Implement the Bubble Sort algorithm with delayed swaps for visualization
+    }
+}
