916986caabe20d9aa70b611d83d7be3d932a2941
Rasty Espartero <gemrasty@gmail.com>
2023-12-17 23:52:45 +0800

    Create BidirectionalOddEvenBubbleSort.java

---
+++ Practice Exercises/Bubble Sort/BidirectionalOddEvenBubbleSort.java
@@ -0,0 +1,13 @@
+public class BidirectionalOddEvenBubbleSort {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        bidirectionalOddEvenBubbleSort(array);
+        System.out.println("Sorted Array with Bidirectional Odd-Even Sorting: " + Arrays.toString(array));
+    }
+
+    public static void bidirectionalOddEvenBubbleSort(int[] arr) {
+        // Implement the Bidirectional Bubble Sort algorithm with odd and even index sorting
+    }
+}
