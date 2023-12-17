2981dd5543229121ea64a5148a38368a3e2f02f7
Rasty Espartero <gemrasty@gmail.com>
2023-12-17 23:51:22 +0800

    Create UniqueElementsBubbleSort.java

---
+++ Practice Exercises/Bubble Sort/UniqueElementsBubbleSort.java
@@ -0,0 +1,13 @@
+public class UniqueElementsBubbleSort {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90, 12, 34, 25, 22};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        bubbleSortUniqueElements(array);
+        System.out.println("Sorted Array with Unique Elements: " + Arrays.toString(array));
+    }
+
+    public static void bubbleSortUniqueElements(int[] arr) {
+        // Implement the Bubble Sort algorithm for an array with unique elements
+    }
+}
