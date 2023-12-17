46387a782bef3f9484283c481e29d0162b9bef9e
Rasty Espartero <gemrasty@gmail.com>
2023-12-17 23:47:58 +0800

    Create ThreeWayBubbleSort.java

---
+++ Practice Exercises/Bubble Sort/ThreeWayBubbleSort.java
@@ -0,0 +1,13 @@
+public class ThreeWayBubbleSort {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original Array: " + Arrays.toString(array));
+        threeWayBubbleSort(array);
+        System.out.println("Sorted Array with Three-Way Bubble Sort: " + Arrays.toString(array));
+    }
+
+    public static void threeWayBubbleSort(int[] arr) {
+        // Implement the Three-Way Bubble Sort algorithm
+    }
+}
