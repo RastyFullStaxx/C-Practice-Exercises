89ebdb584a79cbfb36eca9774bbb9cd518977a49
Rasty Espartero <gemrasty@gmail.com>
2023-12-18 23:47:16 +0800

    Create VowelConsonantCount.java

---
+++ Practice Exercises/String Methods/VowelConsonantCount.java
@@ -0,0 +1,22 @@
+public class VowelConsonantCount {
+    public static void main(String[] args) {
+        String input = "hello";
+
+        System.out.println("Original String: " + input);
+        int vowelCount = countVowels(input);
+        int consonantCount = countConsonants(input);
+
+        System.out.println("Vowel Count: " + vowelCount);
+        System.out.println("Consonant Count: " + consonantCount);
+    }
+
+    public static int countVowels(String str) {
+        // Implement a method to count the number of vowels in a given string
+        return 0;
+    }
+
+    public static int countConsonants(String str) {
+        // Implement a method to count the number of consonants in a given string
+        return 0;
+    }
+}
