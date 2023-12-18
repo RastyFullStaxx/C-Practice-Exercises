6703d4bc37177794bf99bc7ab6483a6aef8fe3b4
Rasty Espartero <gemrasty@gmail.com>
2023-12-18 23:47:02 +0800

    Create PalindromeCheck.java

---
+++ Practice Exercises/String Methods/PalindromeCheck.java
@@ -0,0 +1,14 @@
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        String input = "level";
+
+        System.out.println("Original String: " + input);
+        boolean isPalindrome = isPalindrome(input);
+        System.out.println("Is Palindrome: " + isPalindrome);
+    }
+
+    public static boolean isPalindrome(String str) {
+        // Implement a method to check if a given string is a palindrome
+        return false;
+    }
+}
