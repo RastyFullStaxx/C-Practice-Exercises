ee085b7a56da9a07048ac5063fff58882dc859df
Rasty Espartero <gemrasty@gmail.com>
2023-12-18 23:46:50 +0800

    Create ReverseString.java

---
+++ Practice Exercises/String Methods/ReverseString.java
@@ -0,0 +1,14 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String input = "hello";
+
+        System.out.println("Original String: " + input);
+        String reversedString = reverseString(input);
+        System.out.println("Reversed String: " + reversedString);
+    }
+
+    public static String reverseString(String str) {
+        // Implement a method to reverse a given string
+        return null;
+    }
+}
