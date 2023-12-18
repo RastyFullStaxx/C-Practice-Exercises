c700ef5b6c9329d81531018bf180c086b38ee051
Rasty Espartero <gemrasty@gmail.com>
2023-12-18 23:48:09 +0800

    Create StringComparison.java

---
+++ Practice Exercises/String Methods/StringComparison.java
@@ -0,0 +1,17 @@
+public class StringComparison {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "hello";
+
+        System.out.println("String 1: " + str1);
+        System.out.println("String 2: " + str2);
+
+        boolean areEqual = compareStrings(str1, str2);
+        System.out.println("Strings are Equal: " + areEqual);
+    }
+
+    public static boolean compareStrings(String str1, String str2) {
+        // Implement a method to compare two strings for equality (case-sensitive)
+        return false;
+    }
+}
