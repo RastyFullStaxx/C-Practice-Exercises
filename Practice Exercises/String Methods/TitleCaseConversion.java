5a28fa19f9ae027e29fd3677e0c60b3539c907e1
Rasty Espartero <gemrasty@gmail.com>
2023-12-18 23:48:51 +0800

    Create TitleCaseConversion.java

---
+++ Practice Exercises/String Methods/TitleCaseConversion.java
@@ -0,0 +1,14 @@
+public class TitleCaseConversion {
+    public static void main(String[] args) {
+        String input = "hello world";
+
+        System.out.println("Original String: " + input);
+        String titleCaseString = convertToTitleCase(input);
+        System.out.println("Title Case String: " + titleCaseString);
+    }
+
+    public static String convertToTitleCase(String str) {
+        // Implement a method to convert a given string to title case
+        return null;
+    }
+}
