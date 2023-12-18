a5c1fa05ba70f93327470ab67dedf375be5e7d98
Rasty Espartero <gemrasty@gmail.com>
2023-12-18 23:48:00 +0800

    Create SubstringSearch.java

---
+++ Practice Exercises/String Methods/SubstringSearch.java
@@ -0,0 +1,17 @@
+public class SubstringSearch {
+    public static void main(String[] args) {
+        String input = "Hello World";
+        String substring = "World";
+
+        System.out.println("Original String: " + input);
+        System.out.println("Substring to Find: " + substring);
+
+        boolean containsSubstring = containsSubstring(input, substring);
+        System.out.println("Contains Substring: " + containsSubstring);
+    }
+
+    public static boolean containsSubstring(String str, String substring) {
+        // Implement a method to check if a given string contains a specified substring
+        return false;
+    }
+}
