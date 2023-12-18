e586a16a565f64241f31c3545e6e6035420a8699
Rasty Espartero <gemrasty@gmail.com>
2023-12-18 23:47:28 +0800

    Create CaseConversion.java

---
+++ Practice Exercises/String Methods/CaseConversion.java
@@ -0,0 +1,22 @@
+public class CaseConversion {
+    public static void main(String[] args) {
+        String input = "Hello World";
+
+        System.out.println("Original String: " + input);
+        String uppercaseString = convertToUppercase(input);
+        String lowercaseString = convertToLowercase(input);
+
+        System.out.println("Uppercase: " + uppercaseString);
+        System.out.println("Lowercase: " + lowercaseString);
+    }
+
+    public static String convertToUppercase(String str) {
+        // Implement a method to convert a given string to uppercase
+        return null;
+    }
+
+    public static String convertToLowercase(String str) {
+        // Implement a method to convert a given string to lowercase
+        return null;
+    }
+}
