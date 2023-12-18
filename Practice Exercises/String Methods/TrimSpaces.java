dd43638cbca7faefd8be71469d76bad8e9f408c8
Rasty Espartero <gemrasty@gmail.com>
2023-12-18 23:48:38 +0800

    Create TrimSpaces.java

---
+++ Practice Exercises/String Methods/TrimSpaces.java
@@ -0,0 +1,14 @@
+public class TrimSpaces {
+    public static void main(String[] args) {
+        String input = "   Hello World   ";
+
+        System.out.println("Original String: " + input);
+        String trimmedString = trimSpaces(input);
+        System.out.println("Trimmed String: " + trimmedString);
+    }
+
+    public static String trimSpaces(String str) {
+        // Implement a method to trim leading and trailing spaces from a given string
+        return null;
+    }
+}
