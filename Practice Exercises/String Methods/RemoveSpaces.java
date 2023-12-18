3ee0af01e4d917a1e773c20e2eb8ff23fa50a60b
Rasty Espartero <gemrasty@gmail.com>
2023-12-18 23:47:38 +0800

    Create RemoveSpaces.java

---
+++ Practice Exercises/String Methods/RemoveSpaces.java
@@ -0,0 +1,14 @@
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String input = "  Hello   World  ";
+
+        System.out.println("Original String: " + input);
+        String stringWithoutSpaces = removeSpaces(input);
+        System.out.println("String without Spaces: " + stringWithoutSpaces);
+    }
+
+    public static String removeSpaces(String str) {
+        // Implement a method to remove spaces from a given string
+        return null;
+    }
+}
