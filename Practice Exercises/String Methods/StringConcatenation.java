009eb82040d08fc63ad59211b3da0de79b23fa45
Rasty Espartero <gemrasty@gmail.com>
2023-12-18 23:47:49 +0800

    Create StringConcatenation.java

---
+++ Practice Exercises/String Methods/StringConcatenation.java
@@ -0,0 +1,17 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = " World";
+
+        System.out.println("String 1: " + str1);
+        System.out.println("String 2: " + str2);
+
+        String concatenatedString = concatenateStrings(str1, str2);
+        System.out.println("Concatenated String: " + concatenatedString);
+    }
+
+    public static String concatenateStrings(String str1, String str2) {
+        // Implement a method to concatenate two given strings
+        return null;
+    }
+}
