299fe4e8fe4cd3f5b7c463a1b6212575fbd3aa5c
Rasty Espartero <gemrasty@gmail.com>
2023-12-18 23:48:21 +0800

    Create ReplaceSubstring.java

---
+++ Practice Exercises/String Methods/ReplaceSubstring.java
@@ -0,0 +1,19 @@
+public class ReplaceSubstring {
+    public static void main(String[] args) {
+        String input = "Hello World";
+        String substringToReplace = "World";
+        String replacement = "Universe";
+
+        System.out.println("Original String: " + input);
+        System.out.println("Substring to Replace: " + substringToReplace);
+        System.out.println("Replacement String: " + replacement);
+
+        String replacedString = replaceSubstring(input, substringToReplace, replacement);
+        System.out.println("String after Replacement: " + replacedString);
+    }
+
+    public static String replaceSubstring(String str, String oldSubstring, String newSubstring) {
+        // Implement a method to replace a specified substring with another string
+        return null;
+    }
+}
