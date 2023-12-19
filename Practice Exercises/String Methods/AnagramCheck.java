ce49a1a82c1d41ddc5cc6001e11df812513313bb
Rasty Espartero <gemrasty@gmail.com>
2023-12-19 23:24:05 +0800

    Create AnagramCheck.java

---
+++ Practice Exercises/String Methods/AnagramCheck.java
@@ -0,0 +1,16 @@
+public class AnagramCheck {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        System.out.println("String 1: " + str1);
+        System.out.println("String 2: " + str2);
+        boolean areAnagrams = checkAnagrams(str1, str2);
+        System.out.println("Are Anagrams: " + areAnagrams);
+    }
+
+    public static boolean checkAnagrams(String str1, String str2) {
+        // Implement a method to check if two strings are anagrams
+        return false;
+    }
+}
