5e4879bf35edba4757b2cd1188461bcb3decf3b7
Rasty Espartero <gemrasty@gmail.com>
2023-12-19 23:23:08 +0800

    Create ExtractDigits.java

---
+++ Practice Exercises/String Methods/ExtractDigits.java
@@ -0,0 +1,14 @@
+public class ExtractDigits {
+    public static void main(String[] args) {
+        String input = "abc123xyz456";
+
+        System.out.println("Original String: " + input);
+        String extractedDigits = extractDigits(input);
+        System.out.println("Extracted Digits: " + extractedDigits);
+    }
+
+    public static String extractDigits(String str) {
+        // Implement a method to extract and return the digits from a given string
+        return null;
+    }
+}
