70e5a94fedbb5d1bca8aebbe086f60289b9c01ed
Rasty Espartero <gemrasty@gmail.com>
2023-12-19 23:22:58 +0800

    Create CustomStringLength.java

---
+++ Practice Exercises/String Methods/CustomStringLength.java
@@ -0,0 +1,14 @@
+public class CustomStringLength {
+    public static void main(String[] args) {
+        String input = "Java Programming";
+
+        System.out.println("Original String: " + input);
+        int customLength = calculateStringLength(input);
+        System.out.println("Custom String Length: " + customLength);
+    }
+
+    public static int calculateStringLength(String str) {
+        // Implement a method to calculate the length of a string without using the length() method
+        return 0;
+    }
+}
