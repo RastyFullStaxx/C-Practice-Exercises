22212550f9402233702c7a1d10b962e0d9669e2a
Rasty Espartero <gemrasty@gmail.com>
2023-12-19 23:24:36 +0800

    Create ReplaceMultipleSpaces.java

---
+++ Practice Exercises/String Methods/ReplaceMultipleSpaces.java
@@ -0,0 +1,14 @@
+public class ReplaceMultipleSpaces {
+    public static void main(String[] args) {
+        String input = "    Hello    World    Java    ";
+
+        System.out.println("Original String: " + input);
+        String stringWithSingleSpace = replaceMultipleSpaces(input);
+        System.out.println("String with Single Space: " + stringWithSingleSpace);
+    }
+
+    public static String replaceMultipleSpaces(String str) {
+        // Implement a method to replace multiple spaces with a single space in a given string
+        return null;
+    }
+}
