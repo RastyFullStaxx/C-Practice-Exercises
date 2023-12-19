9088441df9d422ff4b0a9197ee069ac82b659127
Rasty Espartero <gemrasty@gmail.com>
2023-12-19 23:23:22 +0800

    Create ConcatenateWithDelimiter.java

---
+++ Practice Exercises/String Methods/ConcatenateWithDelimiter.java
@@ -0,0 +1,14 @@
+public class ConcatenateWithDelimiter {
+    public static void main(String[] args) {
+        String[] words = {"Hello", "World", "Java"};
+
+        System.out.println("Original Array: " + Arrays.toString(words));
+        String concatenatedString = concatenateWithDelimiter(words, "-");
+        System.out.println("Concatenated String with Delimiter: " + concatenatedString);
+    }
+
+    public static String concatenateWithDelimiter(String[] strings, String delimiter) {
+        // Implement a method to concatenate an array of strings with a specified delimiter
+        return null;
+    }
+}
