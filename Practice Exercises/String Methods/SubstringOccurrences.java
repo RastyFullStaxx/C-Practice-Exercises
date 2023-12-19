5ad435538838fd5a5db8fe43f0c6b98b131926cf
Rasty Espartero <gemrasty@gmail.com>
2023-12-19 23:23:33 +0800

    Create SubstringOccurrences.java

---
+++ Practice Exercises/String Methods/SubstringOccurrences.java
@@ -0,0 +1,14 @@
+public class SubstringOccurrences {
+    public static void main(String[] args) {
+        String input = "hello world hello java hello";
+
+        System.out.println("Original String: " + input);
+        int occurrences = countSubstringOccurrences(input, "hello");
+        System.out.println("Occurrences of 'hello': " + occurrences);
+    }
+
+    public static int countSubstringOccurrences(String str, String substring) {
+        // Implement a method to count the occurrences of a substring in a given string
+        return 0;
+    }
+}
