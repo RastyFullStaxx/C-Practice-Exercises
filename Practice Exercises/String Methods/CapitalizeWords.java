034594a1b8242d5e2294782db13acadf4b5003a5
Rasty Espartero <gemrasty@gmail.com>
2023-12-19 23:23:44 +0800

    Create CapitalizeWords.java

---
+++ Practice Exercises/String Methods/CapitalizeWords.java
@@ -0,0 +1,14 @@
+public class CapitalizeWords {
+    public static void main(String[] args) {
+        String sentence = "java programming is fun";
+
+        System.out.println("Original Sentence: " + sentence);
+        String capitalizedSentence = capitalizeWords(sentence);
+        System.out.println("Capitalized Sentence: " + capitalizedSentence);
+    }
+
+    public static String capitalizeWords(String str) {
+        // Implement a method to capitalize the first letter of each word in a sentence
+        return null;
+    }
+}
