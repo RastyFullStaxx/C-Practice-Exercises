96d6754de3ceff3ac2c9a71358fca7b1637e81d4
Rasty Espartero <gemrasty@gmail.com>
2023-12-19 23:24:14 +0800

    Create StringEncryption.java

---
+++ Practice Exercises/String Methods/StringEncryption.java
@@ -0,0 +1,14 @@
+public class StringEncryption {
+    public static void main(String[] args) {
+        String input = "hello";
+
+        System.out.println("Original String: " + input);
+        String encryptedString = encryptString(input, 3);
+        System.out.println("Encrypted String: " + encryptedString);
+    }
+
+    public static String encryptString(String str, int shift) {
+        // Implement a method to encrypt a string using a Caesar cipher with a specified shift
+        return null;
+    }
+}
