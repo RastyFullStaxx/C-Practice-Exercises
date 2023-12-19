6f21c1be7b1b744cd0169d86f8b457f530ff7ece
Rasty Espartero <gemrasty@gmail.com>
2023-12-19 23:22:49 +0800

    Create RemoveDuplicates.java

---
+++ Practice Exercises/String Methods/RemoveDuplicates.java
@@ -0,0 +1,14 @@
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        String input = "programming";
+
+        System.out.println("Original String: " + input);
+        String stringWithoutDuplicates = removeDuplicateChars(input);
+        System.out.println("String without Duplicates: " + stringWithoutDuplicates);
+    }
+
+    public static String removeDuplicateChars(String str) {
+        // Implement a method to remove duplicate characters from a given string
+        return null;
+    }
+}
