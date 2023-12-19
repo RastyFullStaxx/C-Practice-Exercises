3e6b3fb81057018fa7e3aa2d54182f2082ab8712
Rasty Espartero <gemrasty@gmail.com>
2023-12-19 23:23:56 +0800

    Create ReverseWords.java

---
+++ Practice Exercises/String Methods/ReverseWords.java
@@ -0,0 +1,14 @@
+public class ReverseWords {
+    public static void main(String[] args) {
+        String sentence = "Java Programming is Awesome";
+
+        System.out.println("Original Sentence: " + sentence);
+        String reversedWords = reverseWords(sentence);
+        System.out.println("Sentence with Reversed Words: " + reversedWords);
+    }
+
+    public static String reverseWords(String str) {
+        // Implement a method to reverse the order of words in a sentence
+        return null;
+    }
+}
