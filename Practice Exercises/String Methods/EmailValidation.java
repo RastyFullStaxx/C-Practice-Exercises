361f00431e3375901146c6457e384fef3ae1a6e1
Rasty Espartero <gemrasty@gmail.com>
2023-12-19 23:24:26 +0800

    Create EmailValidation.java

---
+++ Practice Exercises/String Methods/EmailValidation.java
@@ -0,0 +1,14 @@
+public class EmailValidation {
+    public static void main(String[] args) {
+        String email = "user@example.com";
+
+        System.out.println("Email Address: " + email);
+        boolean isValidEmail = validateEmail(email);
+        System.out.println("Is Valid Email: " + isValidEmail);
+    }
+
+    public static boolean validateEmail(String email) {
+        // Implement a method to validate if a given string is a valid email address
+        return false;
+    }
+}
