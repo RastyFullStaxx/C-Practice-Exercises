e9e64ade357c31f953496bf231d9ba9058501ccf
Rasty Espartero <gemrasty@gmail.com>
2023-12-20 23:52:32 +0800

    Create StringToInteger.java

---
+++ Practice Exercises/String Methods/StringToInteger.java
@@ -0,0 +1,14 @@
+public class StringToInteger {
+    public static void main(String[] args) {
+        String str = "123";
+
+        System.out.println("Original String: " + str);
+        int convertedInteger = stringToInteger(str);
+        System.out.println("Converted Integer: " + convertedInteger);
+    }
+
+    public static int stringToInteger(String str) {
+        // Implement a method to convert a string to an integer (atoi)
+        return 0;
+    }
+}
