6bf5a938b1a2310e413cc8c49f21fe9a68cf74e1
Rasty Espartero <gemrasty@gmail.com>
2023-12-20 23:52:23 +0800

    Create LongestCommonPrefix.java

---
+++ Practice Exercises/String Methods/LongestCommonPrefix.java
@@ -0,0 +1,14 @@
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] words = {"flower", "flow", "flight"};
+
+        System.out.println("Array of Words: " + Arrays.toString(words));
+        String commonPrefix = longestCommonPrefix(words);
+        System.out.println("Longest Common Prefix: " + commonPrefix);
+    }
+
+    public static String longestCommonPrefix(String[] words) {
+        // Implement a method to find the longest common prefix among an array of strings
+        return null;
+    }
+}
