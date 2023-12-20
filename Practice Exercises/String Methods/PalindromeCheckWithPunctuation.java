b594f1af61af41b6dd9e1bc65f3135b96525a20b
Rasty Espartero <gemrasty@gmail.com>
2023-12-20 23:51:18 +0800

    Create PalindromeCheckWithPunctuation.java

---
+++ Practice Exercises/String Methods/PalindromeCheckWithPunctuation.java
@@ -0,0 +1,14 @@
+public class PalindromeCheckWithPunctuation {
+    public static void main(String[] args) {
+        String input = "A man, a plan, a canal: Panama";
+
+        System.out.println("Original String: " + input);
+        boolean isPalindrome = isPalindromeWithPunctuation(input);
+        System.out.println("Is Palindrome with Punctuation: " + isPalindrome);
+    }
+
+    public static boolean isPalindromeWithPunctuation(String str) {
+        // Implement a method to check if a given string is a palindrome, considering punctuation and spaces
+        return false;
+    }
+}
