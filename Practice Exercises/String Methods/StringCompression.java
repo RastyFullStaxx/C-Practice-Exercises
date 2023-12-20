228cb04416fd4cba5048969365f1d83032b94f67
Rasty Espartero <gemrasty@gmail.com>
2023-12-20 23:51:38 +0800

    Create StringCompression.java

---
+++ Practice Exercises/String Methods/StringCompression.java
@@ -0,0 +1,14 @@
+public class StringCompression {
+    public static void main(String[] args) {
+        String input = "aabcccccaaa";
+
+        System.out.println("Original String: " + input);
+        String compressedString = compressString(input);
+        System.out.println("Compressed String: " + compressedString);
+    }
+
+    public static String compressString(String str) {
+        // Implement a method to perform basic string compression using the counts of repeated characters
+        return null;
+    }
+}
