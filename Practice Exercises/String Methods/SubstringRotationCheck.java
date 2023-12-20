1b4bf0b465052a15153d42de80226b38b1120110
Rasty Espartero <gemrasty@gmail.com>
2023-12-20 23:51:56 +0800

    Create SubstringRotationCheck.java

---
+++ Practice Exercises/String Methods/SubstringRotationCheck.java
@@ -0,0 +1,16 @@
+public class SubstringRotationCheck {
+    public static void main(String[] args) {
+        String str1 = "waterbottle";
+        String str2 = "erbottlewat";
+
+        System.out.println("String 1: " + str1);
+        System.out.println("String 2: " + str2);
+        boolean isRotation = isSubstringRotation(str1, str2);
+        System.out.println("Is Substring Rotation: " + isRotation);
+    }
+
+    public static boolean isSubstringRotation(String str1, String str2) {
+        // Implement a method to check if one string is a rotation of another using only one call to isSubstring
+        return false;
+    }
+}
