2de170276d38c2523dd631e7ca32e9a77b948ff8
Rasty Espartero <gemrasty@gmail.com>
2023-12-20 23:52:15 +0800

    Create ReverseVowels.java

---
+++ Practice Exercises/String Methods/ReverseVowels.java
@@ -0,0 +1,14 @@
+public class ReverseVowels {
+    public static void main(String[] args) {
+        String input = "hello";
+
+        System.out.println("Original String: " + input);
+        String reversedVowels = reverseVowels(input);
+        System.out.println("String with Reversed Vowels: " + reversedVowels);
+    }
+
+    public static String reverseVowels(String str) {
+        // Implement a method to reverse the order of vowels in a given string
+        return null;
+    }
+}
