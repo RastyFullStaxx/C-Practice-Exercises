669c2c7c4ab7ccf9416841e8cd8e10105dfccd2a
Rasty Espartero <gemrasty@gmail.com>
2023-12-20 23:51:29 +0800

    Create CamelCaseSplit.java

---
+++ Practice Exercises/String Methods/CamelCaseSplit.java
@@ -0,0 +1,14 @@
+public class CamelCaseSplit {
+    public static void main(String[] args) {
+        String camelCaseString = "splitCamelCase";
+
+        System.out.println("CamelCase String: " + camelCaseString);
+        String[] words = splitCamelCase(camelCaseString);
+        System.out.println("Split Words: " + Arrays.toString(words));
+    }
+
+    public static String[] splitCamelCase(String str) {
+        // Implement a method to split a CamelCase string into separate words
+        return null;
+    }
+}
