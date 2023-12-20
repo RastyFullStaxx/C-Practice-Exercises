84aada7fdd3012667ca6dd3c7cbf1601b8463233
Rasty Espartero <gemrasty@gmail.com>
2023-12-20 23:51:47 +0800

    Create RemoveDuplicatesFromSorted.java

---
+++ Practice Exercises/String Methods/RemoveDuplicatesFromSorted.java
@@ -0,0 +1,14 @@
+public class RemoveDuplicatesFromSorted {
+    public static void main(String[] args) {
+        String sortedString = "aabbcccdd";
+
+        System.out.println("Sorted String: " + sortedString);
+        String stringWithoutDuplicates = removeDuplicatesFromSorted(sortedString);
+        System.out.println("String without Duplicates: " + stringWithoutDuplicates);
+    }
+
+    public static String removeDuplicatesFromSorted(String str) {
+        // Implement a method to remove duplicates from a sorted string
+        return null;
+    }
+}
