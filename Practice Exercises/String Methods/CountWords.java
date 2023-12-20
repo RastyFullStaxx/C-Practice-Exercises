9483e9c2300ec4c0291807360b7816416b0934b7
Rasty Espartero <gemrasty@gmail.com>
2023-12-20 23:52:05 +0800

    Create CountWords.java

---
+++ Practice Exercises/String Methods/CountWords.java
@@ -0,0 +1,14 @@
+public class CountWords {
+    public static void main(String[] args) {
+        String sentence = "Java programming is versatile and powerful";
+
+        System.out.println("Original Sentence: " + sentence);
+        int wordCount = countWords(sentence);
+        System.out.println("Word Count: " + wordCount);
+    }
+
+    public static int countWords(String str) {
+        // Implement a method to count the number of words in a sentence
+        return 0;
+    }
+}
