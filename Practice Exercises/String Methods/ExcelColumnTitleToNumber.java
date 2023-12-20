4dbc0c4a39004d92b49e2938a5a9b5f8a3d8737c
Rasty Espartero <gemrasty@gmail.com>
2023-12-20 23:52:43 +0800

    Create ExcelColumnTitleToNumber.java

---
+++ Practice Exercises/String Methods/ExcelColumnTitleToNumber.java
@@ -0,0 +1,14 @@
+public class ExcelColumnTitleToNumber {
+    public static void main(String[] args) {
+        String columnTitle = "AB";
+
+        System.out.println("Excel Column Title: " + columnTitle);
+        int columnNumber = titleToNumber(columnTitle);
+        System.out.println("Corresponding Column Number: " + columnNumber);
+    }
+
+    public static int titleToNumber(String columnTitle) {
+        // Implement a method to convert an Excel column title to the corresponding column number
+        return 0;
+    }
+}
