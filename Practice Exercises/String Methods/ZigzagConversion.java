bb22a5c4f3f51453851b9d10f1ae53f4a9979a96
Rasty Espartero <gemrasty@gmail.com>
2023-12-21 23:42:57 +0800

    Create ZigzagConversion.java

---
+++ Practice Exercises/String Methods/ZigzagConversion.java
@@ -0,0 +1,15 @@
+public class ZigzagConversion {
+    public static void main(String[] args) {
+        String input = "PAYPALISHIRING";
+        int numRows = 3;
+
+        System.out.println("Original String: " + input);
+        String zigzagConversion = convertToZigzag(input, numRows);
+        System.out.println("Zigzag Conversion: " + zigzagConversion);
+    }
+
+    public static String convertToZigzag(String str, int numRows) {
+        // Implement a method to perform zigzag conversion on a given string
+        return null;
+    }
+}
