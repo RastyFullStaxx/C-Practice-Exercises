df2b0f24a448ce16b2a9ebad9ea0e7bd5465dba5
Rasty Espartero <gemrasty@gmail.com>
2023-12-21 23:44:01 +0800

    Create AddBinary.java

---
+++ Practice Exercises/String Methods/AddBinary.java
@@ -0,0 +1,16 @@
+public class AddBinary {
+    public static void main(String[] args) {
+        String a = "1010";
+        String b = "1011";
+
+        System.out.println("Binary String A: " + a);
+        System.out.println("Binary String B: " + b);
+        String sum = addBinary(a, b);
+        System.out.println("Binary Sum: " + sum);
+    }
+
+    public static String addBinary(String a, String b) {
+        // Implement a method to add two binary strings
+        return null;
+    }
+}
