0cd79c51f4d52d4bfefd121cbdb2be986fe83798
Rasty Espartero <gemrasty@gmail.com>
2023-12-21 23:43:31 +0800

    Create ImplementStrStr.java

---
+++ Practice Exercises/String Methods/ImplementStrStr.java
@@ -0,0 +1,16 @@
+public class ImplementStrStr {
+    public static void main(String[] args) {
+        String haystack = "hello";
+        String needle = "ll";
+
+        System.out.println("Haystack: " + haystack);
+        System.out.println("Needle: " + needle);
+        int index = strStr(haystack, needle);
+        System.out.println("Index of Needle in Haystack: " + index);
+    }
+
+    public static int strStr(String haystack, String needle) {
+        // Implement a method to find the index of the first occurrence of needle in haystack
+        return -1;
+    }
+}
