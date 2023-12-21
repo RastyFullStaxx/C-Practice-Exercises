3411420fc906a3deb72f4bf4e79d3892ec381d8b
Rasty Espartero <gemrasty@gmail.com>
2023-12-21 23:44:09 +0800

    Create ValidPalindrome.java

---
+++ Practice Exercises/String Methods/ValidPalindrome.java
@@ -0,0 +1,14 @@
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        String input = "A man, a plan, a canal: Panama";
+
+        System.out.println("Original String: " + input);
+        boolean isValid = isPalindrome(input);
+        System.out.println("Is Valid Palindrome: " + isValid);
+    }
+
+    public static boolean isPalindrome(String s) {
+        // Implement a method to check if a given string is a valid palindrome
+        return false;
+    }
+}
