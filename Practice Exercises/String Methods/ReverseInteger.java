479fd5e431b3fe81a754747a62348fb5a7f4de54
Rasty Espartero <gemrasty@gmail.com>
2023-12-21 23:43:41 +0800

    Create ReverseInteger.java

---
+++ Practice Exercises/String Methods/ReverseInteger.java
@@ -0,0 +1,14 @@
+public class ReverseInteger {
+    public static void main(String[] args) {
+        int num = 123;
+
+        System.out.println("Original Integer: " + num);
+        int reversedInteger = reverse(num);
+        System.out.println("Reversed Integer: " + reversedInteger);
+    }
+
+    public static int reverse(int x) {
+        // Implement a method to reverse the digits of an integer
+        return 0;
+    }
+}
