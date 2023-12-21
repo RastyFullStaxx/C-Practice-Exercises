4820b914a58c3f5ced3906bf7e16f17cb2d4009f
Rasty Espartero <gemrasty@gmail.com>
2023-12-21 23:43:08 +0800

    Create ValidParentheses.java

---
+++ Practice Exercises/String Methods/ValidParentheses.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String parentheses = "{[()]}";
+
+        System.out.println("Parentheses: " + parentheses);
+        boolean isValid = isValidParentheses(parentheses);
+        System.out.println("Is Valid: " + isValid);
+    }
+
+    public static boolean isValidParentheses(String s) {
+        // Implement a method to check if a given string of parentheses is valid
+        return false;
+    }
+}
