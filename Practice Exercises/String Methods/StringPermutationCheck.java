56b79ed11d368f6b11f086008b237d47bf3efae6
Rasty Espartero <gemrasty@gmail.com>
2023-12-21 23:42:46 +0800

    Create StringPermutationCheck.java

---
+++ Practice Exercises/String Methods/StringPermutationCheck.java
@@ -0,0 +1,16 @@
+public class StringPermutationCheck {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        System.out.println("String 1: " + str1);
+        System.out.println("String 2: " + str2);
+        boolean arePermutations = checkPermutation(str1, str2);
+        System.out.println("Are Permutations: " + arePermutations);
+    }
+
+    public static boolean checkPermutation(String str1, String str2) {
+        // Implement a method to check if two strings are permutations of each other
+        return false;
+    }
+}
