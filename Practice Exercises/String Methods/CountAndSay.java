d45f2c674e85669b1835eb6ce15f1a31eee1be48
Rasty Espartero <gemrasty@gmail.com>
2023-12-21 23:43:19 +0800

    Create CountAndSay.java

---
+++ Practice Exercises/String Methods/CountAndSay.java
@@ -0,0 +1,12 @@
+public class CountAndSay {
+    public static void main(String[] args) {
+        int n = 4;
+
+        System.out.println("Count and Say for n=" + n + ": " + countAndSay(n));
+    }
+
+    public static String countAndSay(int n) {
+        // Implement a method to generate the nth term of the count-and-say sequence
+        return null;
+    }
+}
