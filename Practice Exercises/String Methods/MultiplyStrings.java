090a19ad92fb4cada9ef57a26f5228fa22f10bf8
Rasty Espartero <gemrasty@gmail.com>
2023-12-22 23:27:27 +0800

    Create MultiplyStrings.java

---
+++ Practice Exercises/String Methods/MultiplyStrings.java
@@ -0,0 +1,16 @@
+public class MultiplyStrings {
+    public static void main(String[] args) {
+        String num1 = "123";
+        String num2 = "456";
+
+        System.out.println("Number 1: " + num1);
+        System.out.println("Number 2: " + num2);
+        String product = multiplyStrings(num1, num2);
+        System.out.println("Product: " + product);
+    }
+
+    public static String multiplyStrings(String num1, String num2) {
+        // Implement a method to multiply two non-negative integers represented as strings
+        return null;
+    }
+}
