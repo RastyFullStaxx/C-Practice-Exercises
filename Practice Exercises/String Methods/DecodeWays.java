838265dbf6a3cec1880f5d8f6d1b678d3e78bd97
Rasty Espartero <gemrasty@gmail.com>
2023-12-22 23:27:37 +0800

    Create DecodeWays.java

---
+++ Practice Exercises/String Methods/DecodeWays.java
@@ -0,0 +1,14 @@
+public class DecodeWays {
+    public static void main(String[] args) {
+        String s = "226";
+
+        System.out.println("String s: " + s);
+        int ways = numDecodings(s);
+        System.out.println("Number of Decodings: " + ways);
+    }
+
+    public static int numDecodings(String s) {
+        // Implement a method to count the number of ways to decode a given string
+        return 0;
+    }
+}
