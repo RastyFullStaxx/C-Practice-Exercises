b68b39efcdf4c3a731b1a87ce2bf5f800d47e6d5
Rasty Espartero <gemrasty@gmail.com>
2023-12-22 23:27:45 +0800

    Create WildcardMatching.java

---
+++ Practice Exercises/String Methods/WildcardMatching.java
@@ -0,0 +1,16 @@
+public class WildcardMatching {
+    public static void main(String[] args) {
+        String s = "adceb";
+        String p = "*a*b";
+
+        System.out.println("String s: " + s);
+        System.out.println("Pattern p: " + p);
+        boolean isMatch = isWildcardMatch(s, p);
+        System.out.println("Is Wildcard Match: " + isMatch);
+    }
+
+    public static boolean isWildcardMatch(String s, String p) {
+        // Implement a method to check if a string matches a wildcard pattern
+        return false;
+    }
+}
