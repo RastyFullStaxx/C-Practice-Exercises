3423c696fec51032ae1512bc64380a0c6880b818
Rasty Espartero <gemrasty@gmail.com>
2023-12-22 23:28:37 +0800

    Create SimplifyPath.java

---
+++ Practice Exercises/String Methods/SimplifyPath.java
@@ -0,0 +1,14 @@
+public class SimplifyPath {
+    public static void main(String[] args) {
+        String path = "/a//b/c/";
+
+        System.out.println("Original Path: " + path);
+        String simplifiedPath = simplifyPath(path);
+        System.out.println("Simplified Path: " + simplifiedPath);
+    }
+
+    public static String simplifyPath(String path) {
+        // Implement a method to simplify a Unix-style file path
+        return null;
+    }
+}
