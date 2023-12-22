6faee9117a651b45174e0ce7c8eb29f8ab45e86c
Rasty Espartero <gemrasty@gmail.com>
2023-12-22 23:28:28 +0800

    Create ValidNumber.java

---
+++ Practice Exercises/String Methods/ValidNumber.java
@@ -0,0 +1,14 @@
+public class ValidNumber {
+    public static void main(String[] args) {
+        String s = " 0.1";
+
+        System.out.println("String s: " + s);
+        boolean isValidNumber = isNumber(s);
+        System.out.println("Is Valid Number: " + isValidNumber);
+    }
+
+    public static boolean isNumber(String s) {
+        // Implement a method to check if a given string is a valid number
+        return false;
+    }
+}
