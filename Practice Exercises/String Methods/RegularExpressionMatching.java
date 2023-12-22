5cfb21a6ba1e32b34a20f55150815e0cac7ae5ee
Rasty Espartero <gemrasty@gmail.com>
2023-12-22 23:27:04 +0800

    Create RegularExpressionMatching.java

---
+++ Practice Exercises/String Methods/RegularExpressionMatching.java
@@ -0,0 +1,16 @@
+public class RegularExpressionMatching {
+    public static void main(String[] args) {
+        String s = "aa";
+        String p = "a*";
+
+        System.out.println("String s: " + s);
+        System.out.println("Pattern p: " + p);
+        boolean isMatch = isMatch(s, p);
+        System.out.println("Is Match: " + isMatch);
+    }
+
+    public static boolean isMatch(String s, String p) {
+        // Implement a method to check if a string matches a regular expression pattern
+        return false;
+    }
+}
