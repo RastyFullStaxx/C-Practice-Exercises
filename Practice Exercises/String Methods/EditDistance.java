b24defda3d359f0d891d54b814686da115b13c1c
Rasty Espartero <gemrasty@gmail.com>
2023-12-23 23:51:42 +0800

    Create EditDistance.java

---
+++ Practice Exercises/String Methods/EditDistance.java
@@ -0,0 +1,16 @@
+public class EditDistance {
+    public static void main(String[] args) {
+        String word1 = "horse";
+        String word2 = "ros";
+
+        System.out.println("Word 1: " + word1);
+        System.out.println("Word 2: " + word2);
+        int distance = minDistance(word1, word2);
+        System.out.println("Edit Distance: " + distance);
+    }
+
+    public static int minDistance(String word1, String word2) {
+        // Implement a method to compute the edit distance between two words
+        return 0;
+    }
+}
