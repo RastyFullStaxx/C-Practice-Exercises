3aa80f5e1c8a15ac356b5da63b8810fe75737ada
Rasty Espartero <gemrasty@gmail.com>
2023-12-23 23:50:55 +0800

    Create LongestSubstringWithoutRepeating.java

---
+++ Practice Exercises/String Methods/LongestSubstringWithoutRepeating.java
@@ -0,0 +1,14 @@
+public class LongestSubstringWithoutRepeating {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+
+        System.out.println("String s: " + s);
+        int length = lengthOfLongestSubstring(s);
+        System.out.println("Length of Longest Substring Without Repeating Characters: " + length);
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        // Implement a method to find the length of the longest substring without repeating characters
+        return 0;
+    }
+}
