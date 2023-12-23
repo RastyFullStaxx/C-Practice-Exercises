236231535ffe16f5d234bc76f3255a22e469056f
Rasty Espartero <gemrasty@gmail.com>
2023-12-23 23:51:04 +0800

    Create PalindromePairs.java

---
+++ Practice Exercises/String Methods/PalindromePairs.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class PalindromePairs {
+    public static void main(String[] args) {
+        String[] words = {"bat", "tab", "cat"};
+
+        System.out.println("Array of Words: " + Arrays.toString(words));
+        List<List<Integer>> palindromePairs = findPalindromePairs(words);
+        System.out.println("Palindrome Pairs: " + palindromePairs);
+    }
+
+    public static List<List<Integer>> findPalindromePairs(String[] words) {
+        // Implement a method to find all palindrome pairs in a given array of words
+        return null;
+    }
+}
