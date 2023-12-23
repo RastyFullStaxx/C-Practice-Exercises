9986dc5bafa504f1aee7ccc17bec50cfa30551ce
Rasty Espartero <gemrasty@gmail.com>
2023-12-23 23:52:02 +0800

    Create DecodeString.java

---
+++ Practice Exercises/String Methods/DecodeString.java
@@ -0,0 +1,14 @@
+public class DecodeString {
+    public static void main(String[] args) {
+        String s = "3[a2[c]]";
+
+        System.out.println("String s: " + s);
+        String decodedString = decodeString(s);
+        System.out.println("Decoded String: " + decodedString);
+    }
+
+    public static String decodeString(String s) {
+        // Implement a method to decode a string with a specific pattern
+        return null;
+    }
+}
