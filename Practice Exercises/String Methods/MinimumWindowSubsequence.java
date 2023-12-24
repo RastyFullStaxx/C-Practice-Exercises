1badcc653bf194fb17ced66f6fc409a13ed2493c
Rasty Espartero <gemrasty@gmail.com>
2023-12-24 23:18:16 +0800

    Create MinimumWindowSubsequence.java

---
+++ Practice Exercises/String Methods/MinimumWindowSubsequence.java
@@ -0,0 +1,16 @@
+public class MinimumWindowSubsequence {
+    public static void main(String[] args) {
+        String s = "abcdebdde";
+        String t = "bde";
+
+        System.out.println("String s: " + s);
+        System.out.println("String t: " + t);
+        String minWindow = minWindowSubsequence(s, t);
+        System.out.println("Minimum Window Subsequence: " + minWindow);
+    }
+
+    public static String minWindowSubsequence(String s, String t) {
+        // Implement a method to find the minimum window in s which will contain all the characters in t in complexity O(n)
+        return null;
+    }
+}
