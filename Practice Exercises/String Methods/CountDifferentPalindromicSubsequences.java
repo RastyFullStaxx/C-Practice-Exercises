da675618555e4d7bec44adf5c4317869ae387d9b
Rasty Espartero <gemrasty@gmail.com>
2023-12-24 23:19:21 +0800

    Create CountDifferentPalindromicSubsequences.java

---
+++ Practice Exercises/String Methods/CountDifferentPalindromicSubsequences.java
@@ -0,0 +1,14 @@
+public class CountDifferentPalindromicSubsequences {
+    public static void main(String[] args) {
+        String s = "bccb";
+
+        System.out.println("String s: " + s);
+        int count = countPalindromicSubsequences(s);
+        System.out.println("Count of Different Palindromic Subsequences: " + count);
+    }
+
+    public static int countPalindromicSubsequences(String s) {
+        // Implement a method to count different palindromic subsequences in a given string
+        return 0;
+    }
+}
