1be8695f26541551407029fdb5f7deede36a45c0
Rasty Espartero <gemrasty@gmail.com>
2023-12-24 23:17:33 +0800

    Create ShortestPalindrome.java

---
+++ Practice Exercises/String Methods/ShortestPalindrome.java
@@ -0,0 +1,14 @@
+public class ShortestPalindrome {
+    public static void main(String[] args) {
+        String s = "aacecaaa";
+
+        System.out.println("String s: " + s);
+        String shortestPalindrome = shortestPalindrome(s);
+        System.out.println("Shortest Palindrome: " + shortestPalindrome);
+    }
+
+    public static String shortestPalindrome(String s) {
+        // Implement a method to find and return the shortest palindrome that can be formed by adding characters in front of the given string
+        return null;
+    }
+}
