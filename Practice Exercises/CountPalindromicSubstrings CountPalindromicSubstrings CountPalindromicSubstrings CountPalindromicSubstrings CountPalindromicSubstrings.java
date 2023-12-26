c959981747014fc4cc3ee6230f5ac61c675db863
Rasty Espartero <gemrasty@gmail.com>
2023-12-26 23:25:37 +0800

    Create CountPalindromicSubstrings CountPalindromicSubstrings CountPalindromicSubstrings CountPalindromicSubstrings CountPalindromicSubstrings.java

---
+++ Practice Exercises/CountPalindromicSubstrings CountPalindromicSubstrings CountPalindromicSubstrings CountPalindromicSubstrings CountPalindromicSubstrings.java
@@ -0,0 +1,14 @@
+public class CountPalindromicSubstrings {
+    public static void main(String[] args) {
+        String s = "abc";
+
+        System.out.println("String s: " + s);
+        int countPalindromicSubstrings = countSubstrings(s);
+        System.out.println("Count of Palindromic Substrings: " + countPalindromicSubstrings);
+    }
+
+    public static int countSubstrings(String s) {
+        // Implement a method to count the number of palindromic substrings in a given string
+        return 0;
+    }
+}
