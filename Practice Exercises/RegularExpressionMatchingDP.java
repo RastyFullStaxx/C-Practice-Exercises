ac1699ba7f4cf2bd53c445159ac93e396781bf6f
Rasty Espartero <gemrasty@gmail.com>
2023-12-26 23:22:26 +0800

    Create RegularExpressionMatchingDP.java

---
+++ Practice Exercises/RegularExpressionMatchingDP.java
@@ -0,0 +1,16 @@
+public class RegularExpressionMatchingDP {
+    public static void main(String[] args) {
+        String s = "aa";
+        String p = "a*";
+
+        System.out.println("String s: " + s);
+        System.out.println("Pattern p: " + p);
+        boolean isMatch = isMatchDP(s, p);
+        System.out.println("Is Match (Dynamic Programming): " + isMatch);
+    }
+
+    public static boolean isMatchDP(String s, String p) {
+        // Implement a method to check if a string matches a regular expression pattern using dynamic programming
+        return false;
+    }
+}
