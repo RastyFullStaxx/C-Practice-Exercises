409a7bda658b4aeded3f5a3cb17f576873f7a53a
Rasty Espartero <gemrasty@gmail.com>
2023-12-28 23:15:47 +0800

    Create PalindromePairsListWordsUpdated.java

---
+++ Practice Exercises/Skill Integration/PalindromePairsListWordsUpdated.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class PalindromePairsListWordsUpdated {
+    public static void main(String[] args) {
+        String[] words = {"bat", "tab", "cat"};
+
+        System.out.println("Array of Words: " + Arrays.toString(words));
+        List<List<Integer>> palindromePairs = findPalindromePairsListWordsUpdated(words);
+        System.out.println("Palindrome Pairs in List of Words (Updated): " + palindromePairs);
+    }
+
+    public static List<List<Integer>> findPalindromePairsListWordsUpdated(String[] words) {
+        // Implement an updated method to find all palindrome pairs in a given array of words
+        return null;
+    }
+}
