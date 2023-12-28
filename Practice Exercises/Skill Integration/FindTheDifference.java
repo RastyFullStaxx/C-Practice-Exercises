81c605d08e6b91aff6bd4c3d11836e79203583fa
Rasty Espartero <gemrasty@gmail.com>
2023-12-28 23:11:52 +0800

    Create FindTheDifference.java

---
+++ Practice Exercises/Skill Integration/FindTheDifference.java
@@ -0,0 +1,16 @@
+public class FindTheDifference {
+    public static void main(String[] args) {
+        String s = "abcd";
+        String t = "abcde";
+
+        System.out.println("String s: " + s);
+        System.out.println("String t: " + t);
+        char difference = findTheDifference(s, t);
+        System.out.println("The Difference: " + difference);
+    }
+
+    public static char findTheDifference(String s, String t) {
+        // Implement a method to find the extra character added to string t compared to string s
+        return ' ';
+    }
+}
