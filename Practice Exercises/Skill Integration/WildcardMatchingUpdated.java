883a6ea33e80a1ced02cdd0da6444197ea401b8b
Rasty Espartero <gemrasty@gmail.com>
2023-12-28 23:14:48 +0800

    Create WildcardMatchingUpdated.java

---
+++ Practice Exercises/Skill Integration/WildcardMatchingUpdated.java
@@ -0,0 +1,16 @@
+public class WildcardMatchingUpdated {
+    public static void main(String[] args) {
+        String s = "adceb";
+        String p = "*a*b";
+
+        System.out.println("String s: " + s);
+        System.out.println("Pattern p: " + p);
+        boolean isMatch = isMatchWildcardUpdated(s, p);
+        System.out.println("Is Match (Updated): " + isMatch);
+    }
+
+    public static boolean isMatchWildcardUpdated(String s, String p) {
+        // Implement an updated method to check if a string matches a wildcard pattern using dynamic programming
+        return false;
+    }
+}
