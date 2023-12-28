d8c310295cbbb298cab3d5a152d4029266059ec4
Rasty Espartero <gemrasty@gmail.com>
2023-12-28 23:11:07 +0800

    Create ValidParenthesisStringMaxDepth.java

---
+++ Practice Exercises/Skill Integration/ValidParenthesisStringMaxDepth.java
@@ -0,0 +1,14 @@
+public class ValidParenthesisStringMaxDepth {
+    public static void main(String[] args) {
+        String s = "(*))";
+
+        System.out.println("String s: " + s);
+        boolean isValid = checkValidStringMaxDepth(s);
+        System.out.println("Is Valid Parenthesis String (Max Depth): " + isValid);
+    }
+
+    public static boolean checkValidStringMaxDepth(String s) {
+        // Implement a method to check if a string containing parentheses is valid, considering '*' as a wildcard and max depth of nested parentheses
+        return false;
+    }
+}
