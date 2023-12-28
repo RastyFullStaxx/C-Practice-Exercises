9fc875b539ab780cdd8cd4aac353ba29cf0f5437
Rasty Espartero <gemrasty@gmail.com>
2023-12-28 23:14:18 +0800

    Create ShortestPathBinaryMatrix.java

---
+++ Practice Exercises/Skill Integration/ShortestPathBinaryMatrix.java
@@ -0,0 +1,18 @@
+public class ShortestPathBinaryMatrix {
+    public static void main(String[] args) {
+        int[][] grid = {
+            {0, 1, 0},
+            {0, 0, 0},
+            {1, 1, 0}
+        };
+
+        System.out.println("Grid: " + Arrays.deepToString(grid));
+        int shortestPath = shortestPathBinaryMatrix(grid);
+        System.out.println("Shortest Path in Binary Matrix: " + shortestPath);
+    }
+
+    public static int shortestPathBinaryMatrix(int[][] grid) {
+        // Implement a method to find the shortest path in a binary matrix
+        return 0;
+    }
+}
