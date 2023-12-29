e16e9ad544775dab7a3184519c805a5525e2856f
Rasty Espartero <gemrasty@gmail.com>
2023-12-29 23:55:57 +0800

    Create MinimumASCIIDeleteSum.java

---
+++ Practice Exercises/String Methods/MinimumASCIIDeleteSum.java
@@ -0,0 +1,16 @@
+public class MinimumASCIIDeleteSum {
+    public static void main(String[] args) {
+        String s1 = "sea";
+        String s2 = "eat";
+
+        System.out.println("String s1: " + s1);
+        System.out.println("String s2: " + s2);
+        int minASCIIDeleteSum = minimumDeleteSum(s1, s2);
+        System.out.println("Minimum ASCII Delete Sum: " + minASCIIDeleteSum);
+    }
+
+    public static int minimumDeleteSum(String s1, String s2) {
+        // Implement a method to find the minimum ASCII delete sum for two given strings
+        return 0;
+    }
+}
