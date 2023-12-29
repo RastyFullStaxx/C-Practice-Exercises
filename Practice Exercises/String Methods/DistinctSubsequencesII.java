b319d2639a74f23088e66ffdef3a01b1ffb37a02
Rasty Espartero <gemrasty@gmail.com>
2023-12-29 23:58:08 +0800

    Create DistinctSubsequencesII.java

---
+++ Practice Exercises/String Methods/DistinctSubsequencesII.java
@@ -0,0 +1,14 @@
+public class DistinctSubsequencesII {
+    public static void main(String[] args) {
+        String s = "abc";
+
+        System.out.println("String s: " + s);
+        int distinctSubsequences = distinctSubseqII(s);
+        System.out.println("Distinct Subsequences II: " + distinctSubsequences);
+    }
+
+    public static int distinctSubseqII(String s) {
+        // Implement a method to count the number of distinct subsequences of a string
+        return 0;
+    }
+}
