c5da2a73330429f28c9009a8df6a56746b19726c
Rasty Espartero <gemrasty@gmail.com>
2023-12-29 23:57:54 +0800

    Create AlienDictionary.java

---
+++ Practice Exercises/String Methods/AlienDictionary.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class AlienDictionary {
+    public static void main(String[] args) {
+        String[] words = {"wrt", "wrf", "er", "ett", "rftt"};
+
+        System.out.println("Array of Words: " + Arrays.toString(words));
+        String alienOrder = alienOrder(words);
+        System.out.println("Alien Dictionary Order: " + alienOrder);
+    }
+
+    public static String alienOrder(String[] words) {
+        // Implement a method to determine the order of characters in an alien dictionary
+        return null;
+    }
+}
