7dff60e8dbe22fa76d921943475e7e010cb72fef
Rasty Espartero <gemrasty@gmail.com>
2023-12-29 23:57:43 +0800

    Create PalindromicSubstringsUpdated.java

---
+++ Practice Exercises/String Methods/PalindromicSubstringsUpdated.java
@@ -0,0 +1,14 @@
+public class PalindromicSubstringsUpdated {
+    public static void main(String[] args) {
+        String s = "aaa";
+
+        System.out.println("String s: " + s);
+        int countPalindromicSubstrings = countSubstringsUpdated(s);
+        System.out.println("Count of Palindromic Substrings (Updated): " + countPalindromicSubstrings);
+    }
+
+    public static int countSubstringsUpdated(String s) {
+        // Implement an updated method to count the number of palindromic substrings in a given string
+        return 0;
+    }
+}
