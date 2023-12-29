3f685dedd85d4c4d11268ddce4fb5e6aa32b5fc2
Rasty Espartero <gemrasty@gmail.com>
2023-12-29 23:56:29 +0800

    Create ReverseWordsInStringII.java

---
+++ Practice Exercises/String Methods/ReverseWordsInStringII.java
@@ -0,0 +1,13 @@
+public class ReverseWordsInStringII {
+    public static void main(String[] args) {
+        char[] s = {'t','h','e',' ','s','k','y',' ','i','s',' ','b','l','u','e'};
+
+        System.out.println("Array of Characters: " + Arrays.toString(s));
+        reverseWords(s);
+        System.out.println("Reversed Words in String: " + Arrays.toString(s));
+    }
+
+    public static void reverseWords(char[] s) {
+        // Implement a method to reverse the words in a string in-place
+    }
+}
