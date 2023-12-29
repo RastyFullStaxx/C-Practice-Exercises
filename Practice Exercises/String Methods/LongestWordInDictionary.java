7d1d1d06e25bdc451f6a18314ca143d5459920aa
Rasty Espartero <gemrasty@gmail.com>
2023-12-29 23:56:14 +0800

    Create LongestWordInDictionary.java

---
+++ Practice Exercises/String Methods/LongestWordInDictionary.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class LongestWordInDictionary {
+    public static void main(String[] args) {
+        String[] words = {"w","wo","wor","worl","world"};
+
+        System.out.println("Array of Words: " + Arrays.toString(words));
+        String longestWord = longestWordInDictionary(words);
+        System.out.println("Longest Word in Dictionary: " + longestWord);
+    }
+
+    public static String longestWordInDictionary(String[] words) {
+        // Implement a method to find the longest word in the dictionary that can be built one character at a time
+        return null;
+    }
+}
