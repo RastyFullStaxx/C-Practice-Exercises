abd32a1b9c7866f4e4c8f48b67ed139ae16cf823
Rasty Espartero <gemrasty@gmail.com>
2023-12-29 23:57:13 +0800

    Create ValidPalindromeIV.java

---
+++ Practice Exercises/String Methods/ValidPalindromeIV.java
@@ -0,0 +1,16 @@
+public class ValidPalindromeIII {
+    public static void main(String[] args) {
+        String s = "abcdeca";
+        int k = 2;
+
+        System.out.println("String s: " + s);
+        System.out.println("k: " + k);
+        boolean isValidPalindrome = isValidPalindromeIII(s, k);
+        System.out.println("Is Valid Palindrome with K Deletions: " + isValidPalindrome);
+    }
+
+    public static boolean isValidPalindromeIII(String s, int k) {
+        // Implement a method to check if it's possible to make a valid palindrome by deleting at most k characters
+        return false;
+    }
+}
