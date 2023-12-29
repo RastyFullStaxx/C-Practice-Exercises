9d344bb92f144f1f49586460530387f3b6aca237
Rasty Espartero <gemrasty@gmail.com>
2023-12-29 23:57:26 +0800

    Create ShortestCompletingWord.java

---
+++ Practice Exercises/String Methods/ShortestCompletingWord.java
@@ -0,0 +1,16 @@
+public class ShortestCompletingWord {
+    public static void main(String[] args) {
+        String licensePlate = "1s3 PSt";
+        String[] words = {"step", "steps", "stripe", "stepple"};
+
+        System.out.println("License Plate: " + licensePlate);
+        System.out.println("Array of Words: " + Arrays.toString(words));
+        String shortestCompletingWord = shortestCompletingWord(licensePlate, words);
+        System.out.println("Shortest Completing Word: " + shortestCompletingWord);
+    }
+
+    public static String shortestCompletingWord(String licensePlate, String[] words) {
+        // Implement a method to find the shortest completing word given a license plate and an array of words
+        return null;
+    }
+}
