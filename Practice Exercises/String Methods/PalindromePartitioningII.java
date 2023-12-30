a72034267a77b8a0ddde4853805edb69f3e419ce
Rasty Espartero <gemrasty@gmail.com>
2023-12-30 20:22:10 +0800

    Create PalindromePartitioningII.java

---
+++ Practice Exercises/String Methods/PalindromePartitioningII.java
@@ -0,0 +1,14 @@
+public class PalindromePartitioningII {
+    public static void main(String[] args) {
+        String s = "aab";
+
+        System.out.println("String s: " + s);
+        int minCuts = minCutPalindromePartitioning(s);
+        System.out.println("Minimum Cuts for Palindrome Partitioning: " + minCuts);
+    }
+
+    public static int minCutPalindromePartitioning(String s) {
+        // Implement a method to find the minimum cuts needed for palindrome partitioning
+        return 0;
+    }
+}
