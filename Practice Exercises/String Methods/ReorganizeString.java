6413997c011547f13f2cdda4226407f2205f6eab
Rasty Espartero <gemrasty@gmail.com>
2023-12-30 20:22:21 +0800

    Create ReorganizeString.java

---
+++ Practice Exercises/String Methods/ReorganizeString.java
@@ -0,0 +1,14 @@
+public class ReorganizeString {
+    public static void main(String[] args) {
+        String s = "aab";
+
+        System.out.println("String s: " + s);
+        String reorganizedString = reorganizeString(s);
+        System.out.println("Reorganized String: " + reorganizedString);
+    }
+
+    public static String reorganizeString(String s) {
+        // Implement a method to reorganize the string such that no two adjacent characters are the same
+        return null;
+    }
+}
