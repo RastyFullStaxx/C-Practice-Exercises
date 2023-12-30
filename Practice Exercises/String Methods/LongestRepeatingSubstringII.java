c27b760295fc7e97fa3a8068fbe2cd5ef74578c0
Rasty Espartero <gemrasty@gmail.com>
2023-12-30 20:21:14 +0800

    Create LongestRepeatingSubstringII.java

---
+++ Practice Exercises/String Methods/LongestRepeatingSubstringII.java
@@ -0,0 +1,14 @@
+public class LongestRepeatingSubstring {
+    public static void main(String[] args) {
+        String s = "banana";
+
+        System.out.println("String s: " + s);
+        String longestRepeatingSubstring = longestRepeatingSubstring(s);
+        System.out.println("Longest Repeating Substring: " + longestRepeatingSubstring);
+    }
+
+    public static String longestRepeatingSubstring(String s) {
+        // Implement a method to find the longest repeating substring in a given string
+        return null;
+    }
+}
