f2b72416f79926dfa95aaee77d56b26da1d91f46
Rasty Espartero <gemrasty@gmail.com>
2023-12-30 20:22:54 +0800

    Create LongestValidParenthesesII.java

---
+++ Practice Exercises/String Methods/LongestValidParenthesesII.java
@@ -0,0 +1,14 @@
+public class LongestValidParentheses {
+    public static void main(String[] args) {
+        String s = "(()";
+
+        System.out.println("String s: " + s);
+        int longestValidLength = longestValidParentheses(s);
+        System.out.println("Length of Longest Valid Parentheses: " + longestValidLength);
+    }
+
+    public static int longestValidParentheses(String s) {
+        // Implement a method to find the length of the longest valid parentheses substring
+        return 0;
+    }
+}
