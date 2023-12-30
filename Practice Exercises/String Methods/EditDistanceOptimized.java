c298980a4deb8c409b7d500f154cb0015430a03a
Rasty Espartero <gemrasty@gmail.com>
2023-12-30 20:23:20 +0800

    Create EditDistanceOptimized.java

---
+++ Practice Exercises/String Methods/EditDistanceOptimized.java
@@ -0,0 +1,16 @@
+public class EditDistanceOptimized {
+    public static void main(String[] args) {
+        String word1 = "horse";
+        String word2 = "ros";
+
+        System.out.println("Word 1: " + word1);
+        System.out.println("Word 2: " + word2);
+        int editDistance = minDistanceEditDistanceOptimized(word1, word2);
+        System.out.println("Minimum Edit Distance (Optimized): " + editDistance);
+    }
+
+    public static int minDistanceEditDistanceOptimized(String word1, String word2) {
+        // Implement an optimized method to find the minimum edit distance between two words
+        return 0;
+    }
+}
