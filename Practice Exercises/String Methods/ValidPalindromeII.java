e2861fcb0eb6c4be28a9e2d94cd2be0f04abf835
Rasty Espartero <gemrasty@gmail.com>
2023-12-31 22:37:39 +0800

    Create ValidPalindromeII.java

---
+++ Practice Exercises/String Methods/ValidPalindromeII.java
@@ -0,0 +1,14 @@
+public class ValidPalindromeII {
+    public static void main(String[] args) {
+        String s = "abca";
+
+        System.out.println("String s: " + s);
+        boolean isValidPalindrome = validPalindromeII(s);
+        System.out.println("Is Valid Palindrome with At Most One Deletion: " + isValidPalindrome);
+    }
+
+    public static boolean validPalindromeII(String s) {
+        // Implement a method to check if it's possible to make a valid palindrome by deleting at most one character
+        return false;
+    }
+}
