ea84ebbf366243e1446745819e76be0ed410f3fd
Rasty Espartero <gemrasty@gmail.com>
2023-12-31 22:37:12 +0800

    Create CompareVersionNumbers.java

---
+++ Practice Exercises/String Methods/CompareVersionNumbers.java
@@ -0,0 +1,16 @@
+public class CompareVersionNumbers {
+    public static void main(String[] args) {
+        String version1 = "1.0.1";
+        String version2 = "1";
+
+        System.out.println("Version 1: " + version1);
+        System.out.println("Version 2: " + version2);
+        int comparisonResult = compareVersion(version1, version2);
+        System.out.println("Comparison Result: " + comparisonResult);
+    }
+
+    public static int compareVersion(String version1, String version2) {
+        // Implement a method to compare two version numbers
+        return 0;
+    }
+}
