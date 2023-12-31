d0db07f4288fd1c7e27fc6f08f202905ef87dd98
Rasty Espartero <gemrasty@gmail.com>
2023-12-31 22:36:43 +0800

    Create PalindromePairsListWordsOptimized.java

---
+++ Practice Exercises/String Methods/PalindromePairsListWordsOptimized.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class PalindromePairsListWordsOptimized {
+    public static void main(String[] args) {
+        String[] words = {"bat", "tab", "cat"};
+
+        System.out.println("Array of Words: " + Arrays.toString(words));
+        List<List<Integer>> palindromePairs = findPalindromePairsListWordsOptimized(words);
+        System.out.println("Palindrome Pairs in List of Words (Optimized): " + palindromePairs);
+    }
+
+    public static List<List<Integer>> findPalindromePairsListWordsOptimized(String[] words) {
+        // Implement an optimized method to find all palindrome pairs in a given array of words
+        return null;
+    }
+}
