6b705fbd9d073fbd059c34e9c401437b917e8a4a
Rasty Espartero <gemrasty@gmail.com>
2023-12-31 22:36:13 +0800

    Create GroupShiftedStringsII.java

---
+++ Practice Exercises/String Methods/GroupShiftedStringsII.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class GroupShiftedStrings {
+    public static void main(String[] args) {
+        String[] strings = {"abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"};
+
+        System.out.println("Array of Strings: " + Arrays.toString(strings));
+        List<List<String>> groupedStrings = groupShiftedStrings(strings);
+        System.out.println("Grouped Shifted Strings: " + groupedStrings);
+    }
+
+    public static List<List<String>> groupShiftedStrings(String[] strings) {
+        // Implement a method to group shifted strings together
+        return null;
+    }
+}
