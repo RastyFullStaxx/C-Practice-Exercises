334323b149fc01db298381a5a7ff69db8411a70a
Rasty Espartero <gemrasty@gmail.com>
2023-12-31 22:38:24 +0800

    Create CountBinarySubstringsII.java

---
+++ Practice Exercises/String Methods/CountBinarySubstringsII.java
@@ -0,0 +1,14 @@
+public class CountBinarySubstrings {
+    public static void main(String[] args) {
+        String s = "00110011";
+
+        System.out.println("String s: " + s);
+        int countSubstrings = countBinarySubstrings(s);
+        System.out.println("Count of Binary Substrings: " + countSubstrings);
+    }
+
+    public static int countBinarySubstrings(String s) {
+        // Implement a method to count the number of non-empty (contiguous) substrings with equal number of 0's and 1's
+        return 0;
+    }
+}
