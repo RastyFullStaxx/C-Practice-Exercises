4ad464e676ed6040807223bb469dd705aae4b0aa
Rasty Espartero <gemrasty@gmail.com>
2023-12-31 22:37:29 +0800

    Create LengthOfLastWord.java

---
+++ Practice Exercises/String Methods/LengthOfLastWord.java
@@ -0,0 +1,14 @@
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+        String s = "Hello World";
+
+        System.out.println("String s: " + s);
+        int lengthOfLastWord = lengthOfLastWord(s);
+        System.out.println("Length of Last Word: " + lengthOfLastWord);
+    }
+
+    public static int lengthOfLastWord(String s) {
+        // Implement a method to find the length of the last word in a string
+        return 0;
+    }
+}
