16be4e430e9379241e2a21f4e21707f89e7372c5
Rasty Espartero <gemrasty@gmail.com>
2023-12-31 22:34:48 +0800

    Create ValidParenthesisStringUpdatedII.java

---
+++ Practice Exercises/String Methods/ValidParenthesisStringUpdatedII.java
@@ -0,0 +1,14 @@
+public class ValidParenthesisStringUpdated {
+    public static void main(String[] args) {
+        String s = "(())((())()()(*)(*()(())())())()()((()())((()))(*";
+
+        System.out.println("String s: " + s);
+        boolean isValid = checkValidStringUpdated(s);
+        System.out.println("Is Valid Parenthesis String (Updated): " + isValid);
+    }
+
+    public static boolean checkValidStringUpdated(String s) {
+        // Implement an updated method to check if a string containing parentheses is valid, considering '*' as a wildcard
+        return false;
+    }
+}
