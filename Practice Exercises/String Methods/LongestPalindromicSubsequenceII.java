c84745e5b1a9095de9b4d9330778cddf0bc843b8
Rasty Espartero <gemrasty@gmail.com>
2024-01-01 23:38:56 +0800

    Create LongestPalindromicSubsequenceII.java

---
+++ Practice Exercises/String Methods/LongestPalindromicSubsequenceII.java
@@ -0,0 +1,14 @@
+public class LongestPalindromicSubsequence {
+    public static void main(String[] args) {
+        String s = "bbbab";
+
+        System.out.println("String s: " + s);
+        int longestPalindromeLength = longestPalindromicSubsequence(s);
+        System.out.println("Length of Longest Palindromic Subsequence: " + longestPalindromeLength);
+    }
+
+    public static int longestPalindromicSubsequence(String s) {
+        // Implement a method to find the length of the longest palindromic subsequence in a given string
+        return 0;
+    }
+}
