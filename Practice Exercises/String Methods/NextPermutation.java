07e13b0943477e55d9c1ae770629f8bc71baba4c
Rasty Espartero <gemrasty@gmail.com>
2024-01-01 23:37:56 +0800

    Create NextPermutation.java

---
+++ Practice Exercises/String Methods/NextPermutation.java
@@ -0,0 +1,13 @@
+public class NextPermutation {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+
+        System.out.println("Array of Numbers: " + Arrays.toString(nums));
+        nextPermutation(nums);
+        System.out.println("Next Permutation: " + Arrays.toString(nums));
+    }
+
+    public static void nextPermutation(int[] nums) {
+        // Implement a method to find the next lexicographically greater permutation of a list of numbers
+    }
+}
