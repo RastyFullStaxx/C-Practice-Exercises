90acc037f4e247f384575e55662cbf159163fd89
Rasty Espartero <gemrasty@gmail.com>
2024-01-01 23:39:41 +0800

    Create PalindromePartitioningOptimized.java

---
+++ Practice Exercises/String Methods/PalindromePartitioningOptimized.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class PalindromePartitioningOptimized {
+    public static void main(String[] args) {
+        String s = "aab";
+
+        System.out.println("String s: " + s);
+        List<List<String>> palindromePartitions = partitionPalindromeOptimized(s);
+        System.out.println("Palindrome Partitions (Optimized): " + palindromePartitions);
+    }
+
+    public static List<List<String>> partitionPalindromeOptimized(String s) {
+        // Implement an optimized method to partition a string into palindromic substrings
+        return null;
+    }
+}
