9d95fcc37ae7fb686eb1fbe64db52fa2bf2ad85b
Rasty Espartero <gemrasty@gmail.com>
2024-01-01 23:38:24 +0800

    Create RemoveInvalidParentheses.java

---
+++ Practice Exercises/String Methods/RemoveInvalidParentheses.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class RemoveInvalidParentheses {
+    public static void main(String[] args) {
+        String s = "()())()";
+
+        System.out.println("String s: " + s);
+        List<String> validParentheses = removeInvalidParentheses(s);
+        System.out.println("Valid Parentheses: " + validParentheses);
+    }
+
+    public static List<String> removeInvalidParentheses(String s) {
+        // Implement a method to remove the minimum number of invalid parentheses to make the input string valid
+        return null;
+    }
+}
