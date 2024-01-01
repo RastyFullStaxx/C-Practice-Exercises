b713653d1e46df19889a9853a82ad51ed7ee4745
Rasty Espartero <gemrasty@gmail.com>
2024-01-01 23:37:45 +0800

    Create LongestSubstringKDistinct.java

---
+++ Practice Exercises/String Methods/LongestSubstringKDistinct.java
@@ -0,0 +1,16 @@
+public class LongestSubstringKDistinct {
+    public static void main(String[] args) {
+        String s = "eceba";
+        int k = 2;
+
+        System.out.println("String s: " + s);
+        System.out.println("k: " + k);
+        int longestLength = lengthOfLongestSubstringKDistinct(s, k);
+        System.out.println("Length of Longest Substring with At Most K Distinct Characters: " + longestLength);
+    }
+
+    public static int lengthOfLongestSubstringKDistinct(String s, int k) {
+        // Implement a method to find the length of the longest substring with at most k distinct characters
+        return 0;
+    }
+}
