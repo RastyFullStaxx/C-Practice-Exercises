802ab63e746617d4cbcb86c61874609ddd5f6da7
Rasty Espartero <gemrasty@gmail.com>
2024-01-01 23:37:38 +0800

    Create LongestConsecutiveSequenceII.java

---
+++ Practice Exercises/String Methods/LongestConsecutiveSequenceII.java
@@ -0,0 +1,16 @@
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+
+        System.out.println("Array of Numbers: " + Arrays.toString(nums));
+        int longestConsecutive = longestConsecutiveSequence(nums);
+        System.out.println("Length of Longest Consecutive Sequence: " + longestConsecutive);
+    }
+
+    public static int longestConsecutiveSequence(int[] nums) {
+        // Implement a method to find the length of the longest consecutive sequence in an unsorted array of integers
+        return 0;
+    }
+}
