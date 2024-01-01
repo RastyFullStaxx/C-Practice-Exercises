d617575ed654c58c2b31c56612ee11174af1197d
Rasty Espartero <gemrasty@gmail.com>
2024-01-01 23:39:30 +0800

    Create WildcardMatchingDP.java

---
+++ Practice Exercises/String Methods/WildcardMatchingDP.java
@@ -0,0 +1,16 @@
+public class WildcardMatchingDP {
+    public static void main(String[] args) {
+        String s = "adceb";
+        String p = "*a*b";
+
+        System.out.println("String s: " + s);
+        System.out.println("Pattern p: " + p);
+        boolean isMatch = isMatchWildcardMatchingDP(s, p);
+        System.out.println("Is Match (Dynamic Programming): " + isMatch);
+    }
+
+    public static boolean isMatchWildcardMatchingDP(String s, String p) {
+        // Implement a dynamic programming approach to check if a string matches a wildcard pattern
+        return false;
+    }
+}
