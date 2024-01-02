db4e9a7cd31a1150fbceb31b6626c9e8479604c2
Rasty Espartero <gemrasty@gmail.com>
2024-01-02 23:23:02 +0800

    Create LongestPalindromePermutation.java

---
+++ Practice Exercises/Anagrams/LongestPalindromePermutation.java
@@ -0,0 +1,14 @@
+public class LongestPalindromePermutation {
+    public static void main(String[] args) {
+        String s = "abccccdd";
+
+        System.out.println("String s: " + s);
+        int longestLength = longestPalindromePermutation(s);
+        System.out.println("Length of Longest Palindrome Permutation: " + longestLength);
+    }
+
+    public static int longestPalindromePermutation(String s) {
+        // Implement a method to find the length of the longest palindrome that can be obtained by rearranging characters in the string
+        return 0;
+    }
+}
