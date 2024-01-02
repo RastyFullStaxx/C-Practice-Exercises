fe5ba17663df48b8bf073bb6d94010c13678b9e9
Rasty Espartero <gemrasty@gmail.com>
2024-01-02 23:23:21 +0800

    Create MinStepsToConvertString.java

---
+++ Practice Exercises/Anagrams/MinStepsToConvertString.java
@@ -0,0 +1,16 @@
+public class MinStepsToConvertString {
+    public static void main(String[] args) {
+        String s = "leetcode";
+        String t = "practice";
+
+        System.out.println("String s: " + s);
+        System.out.println("String t: " + t);
+        int minSteps = minStepsToConvertString(s, t);
+        System.out.println("Minimum Number of Steps to Convert String: " + minSteps);
+    }
+
+    public static int minStepsToConvertString(String s, String t) {
+        // Implement a method to find the minimum number of steps to convert string s to string t
+        return 0;
+    }
+}
