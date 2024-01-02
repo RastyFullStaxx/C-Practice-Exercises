635a6e1e0c4e126fb65dd936466d5dff203cf874
Rasty Espartero <gemrasty@gmail.com>
2024-01-02 23:22:44 +0800

    Create MinStepsToAnagram.java

---
+++ Practice Exercises/Anagrams/MinStepsToAnagram.java
@@ -0,0 +1,16 @@
+public class MinStepsToAnagram {
+    public static void main(String[] args) {
+        String s = "bab";
+        String t = "aba";
+
+        System.out.println("String s: " + s);
+        System.out.println("String t: " + t);
+        int minSteps = minStepsToMakeAnagram(s, t);
+        System.out.println("Minimum Number of Steps to Make Two Strings Anagram: " + minSteps);
+    }
+
+    public static int minStepsToMakeAnagram(String s, String t) {
+        // Implement a method to find the minimum number of steps to make two strings anagrams
+        return 0;
+    }
+}
