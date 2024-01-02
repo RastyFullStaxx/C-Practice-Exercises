349dfd28676defb6f383df4c4d44106ecf0826fa
Rasty Espartero <gemrasty@gmail.com>
2024-01-02 23:22:26 +0800

    Create LongestSubstringWithoutRepeatingAndAnagrams.java

---
+++ Practice Exercises/Anagrams/LongestSubstringWithoutRepeatingAndAnagrams.java
@@ -0,0 +1,14 @@
+public class LongestSubstringWithoutRepeatingAndAnagrams {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+
+        System.out.println("String s: " + s);
+        int longestLength = lengthOfLongestSubstringAndAnagrams(s);
+        System.out.println("Length of Longest Substring Without Repeating Characters and Anagrams: " + longestLength);
+    }
+
+    public static int lengthOfLongestSubstringAndAnagrams(String s) {
+        // Implement a method to find the length of the longest substring without repeating characters and anagrams
+        return 0;
+    }
+}
