d0bbc451c9316d09b560ce95ec6db6fbfb0ded7c
Rasty Espartero <gemrasty@gmail.com>
2024-01-02 23:22:53 +0800

    Create FindTheDifference.java

---
+++ Practice Exercises/Anagrams/FindTheDifference.java
@@ -0,0 +1,16 @@
+public class FindTheDifference {
+    public static void main(String[] args) {
+        String s = "abcd";
+        String t = "abcde";
+
+        System.out.println("String s: " + s);
+        System.out.println("String t: " + t);
+        char diffChar = findTheDifference(s, t);
+        System.out.println("Difference Character: " + diffChar);
+    }
+
+    public static char findTheDifference(String s, String t) {
+        // Implement a method to find the character that was added in t compared to s
+        return ' ';
+    }
+}
