d62adc0a93c698e63c2f0123fb6677ec88bde220
Rasty Espartero <gemrasty@gmail.com>
2024-01-02 23:22:07 +0800

    Create ValidAnagram.java

---
+++ Practice Exercises/Anagrams/ValidAnagram.java
@@ -0,0 +1,16 @@
+public class ValidAnagram {
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+
+        System.out.println("String s: " + s);
+        System.out.println("String t: " + t);
+        boolean isValidAnagram = isAnagram(s, t);
+        System.out.println("Is Valid Anagram: " + isValidAnagram);
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        // Implement a method to check if two strings are valid anagrams of each other
+        return false;
+    }
+}
