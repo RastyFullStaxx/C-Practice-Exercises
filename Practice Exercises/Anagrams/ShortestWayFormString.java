ada61d85f722b00fc06d1c20279dc66a00d957fc
Rasty Espartero <gemrasty@gmail.com>
2024-01-03 23:03:14 +0800

    Create ShortestWayFormString.java

---
+++ Practice Exercises/Anagrams/ShortestWayFormString.java
@@ -0,0 +1,16 @@
+public class ShortestWayFormString {
+    public static void main(String[] args) {
+        String source = "abc";
+        String target = "abcbc";
+
+        System.out.println("Source String: " + source);
+        System.out.println("Target String: " + target);
+        int shortestSteps = shortestWay(source, target);
+        System.out.println("Shortest Steps to Form Target String: " + shortestSteps);
+    }
+
+    public static int shortestWay(String source, String target) {
+        // Implement a method to find the shortest way to form the target string using subsequences of the source string
+        return 0;
+    }
+}
