1fdb91868cb7fff54484330de59494abee83789f
Rasty Espartero <gemrasty@gmail.com>
2024-01-03 23:02:53 +0800

    Create MinWindowSubstringAnagrams.java

---
+++ Practice Exercises/Anagrams/MinWindowSubstringAnagrams.java
@@ -0,0 +1,16 @@
+public class MinWindowSubstringAnagrams {
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+
+        System.out.println("String s: " + s);
+        System.out.println("String t: " + t);
+        String minWindow = minWindowSubstringAnagrams(s, t);
+        System.out.println("Minimum Window Substring with Anagrams: " + minWindow);
+    }
+
+    public static String minWindowSubstringAnagrams(String s, String t) {
+        // Implement a method to find the minimum window substring of s that contains all characters of t (including anagrams)
+        return null;
+    }
+}
