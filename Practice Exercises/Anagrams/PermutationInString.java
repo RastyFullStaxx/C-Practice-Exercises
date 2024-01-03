57c7a12c9870d0ffeec5a87760362a030307669c
Rasty Espartero <gemrasty@gmail.com>
2024-01-03 23:01:41 +0800

    Create PermutationInString.java

---
+++ Practice Exercises/Anagrams/PermutationInString.java
@@ -0,0 +1,16 @@
+public class PermutationInString {
+    public static void main(String[] args) {
+        String s1 = "ab";
+        String s2 = "eidbaooo";
+
+        System.out.println("String s1: " + s1);
+        System.out.println("String s2: " + s2);
+        boolean isPermutation = checkInclusionPermutation(s1, s2);
+        System.out.println("Is Permutation: " + isPermutation);
+    }
+
+    public static boolean checkInclusionPermutation(String s1, String s2) {
+        // Implement a method to check if s1 is a permutation of s2
+        return false;
+    }
+}
