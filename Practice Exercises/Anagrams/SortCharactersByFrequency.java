866d406638c3f1ea1f3526956b4cbda46c18f5bf
Rasty Espartero <gemrasty@gmail.com>
2024-01-03 23:03:04 +0800

    Create SortCharactersByFrequency.java

---
+++ Practice Exercises/Anagrams/SortCharactersByFrequency.java
@@ -0,0 +1,14 @@
+public class SortCharactersByFrequency {
+    public static void main(String[] args) {
+        String s = "tree";
+
+        System.out.println("String s: " + s);
+        String sortedString = frequencySort(s);
+        System.out.println("Sorted String by Frequency: " + sortedString);
+    }
+
+    public static String frequencySort(String s) {
+        // Implement a method to sort the characters in a string based on their frequency
+        return null;
+    }
+}
