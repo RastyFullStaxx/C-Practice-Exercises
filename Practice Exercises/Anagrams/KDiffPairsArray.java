1f9acf465143c114a57758f24d8cf83ec35d99d9
Rasty Espartero <gemrasty@gmail.com>
2024-01-03 23:02:22 +0800

    Create KDiffPairsArray.java

---
+++ Practice Exercises/Anagrams/KDiffPairsArray.java
@@ -0,0 +1,15 @@
+public class KDiffPairsArray {
+    public static void main(String[] args) {
+        int[] nums = {3, 1, 4, 1, 5};
+        int k = 2;
+
+        System.out.println("Array of Numbers: " + Arrays.toString(nums));
+        int kDiffPairs = findPairsKDiff(nums, k);
+        System.out.println("Number of K-Diff Pairs: " + kDiffPairs);
+    }
+
+    public static int findPairsKDiff(int[] nums, int k) {
+        // Implement a method to find the number of k-diff pairs in an array
+        return 0;
+    }
+}
