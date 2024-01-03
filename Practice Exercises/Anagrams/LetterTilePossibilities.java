3dfc80e3751b57ab091a1cb3c0631b9a0c0266ce
Rasty Espartero <gemrasty@gmail.com>
2024-01-03 23:02:10 +0800

    Create LetterTilePossibilities.java

---
+++ Practice Exercises/Anagrams/LetterTilePossibilities.java
@@ -0,0 +1,14 @@
+public class LetterTilePossibilities {
+    public static void main(String[] args) {
+        String tiles = "AAB";
+
+        System.out.println("String tiles: " + tiles);
+        int possibilities = numTilePossibilities(tiles);
+        System.out.println("Number of Letter Tile Possibilities: " + possibilities);
+    }
+
+    public static int numTilePossibilities(String tiles) {
+        // Implement a method to find the number of different letter tile possibilities
+        return 0;
+    }
+}
