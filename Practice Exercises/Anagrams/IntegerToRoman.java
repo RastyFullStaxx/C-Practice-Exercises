96c7e4f101e8f39209f7b842bcec61c088598134
Rasty Espartero <gemrasty@gmail.com>
2024-01-04 19:54:39 +0800

    Create IntegerToRoman.java

---
+++ Practice Exercises/Anagrams/IntegerToRoman.java
@@ -0,0 +1,14 @@
+public class IntegerToRoman {
+    public static void main(String[] args) {
+        int num = 58;
+
+        System.out.println("Integer: " + num);
+        String romanNumeral = intToRoman(num);
+        System.out.println("Roman Numeral: " + romanNumeral);
+    }
+
+    public static String intToRoman(int num) {
+        // Implement a method to convert an integer to a Roman numeral
+        return null;
+    }
+}
