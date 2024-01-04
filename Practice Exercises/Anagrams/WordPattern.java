a85d07f611e37d2cea333e4d0bdd8288f5fee800
Rasty Espartero <gemrasty@gmail.com>
2024-01-04 19:53:47 +0800

    Create WordPattern.java

---
+++ Practice Exercises/Anagrams/WordPattern.java
@@ -0,0 +1,16 @@
+public class WordPattern {
+    public static void main(String[] args) {
+        String pattern = "abba";
+        String str = "dog cat cat dog";
+
+        System.out.println("Pattern: " + pattern);
+        System.out.println("String: " + str);
+        boolean wordPatternMatch = wordPattern(pattern, str);
+        System.out.println("Does Pattern Match String: " + wordPatternMatch);
+    }
+
+    public static boolean wordPattern(String pattern, String str) {
+        // Implement a method to check if a pattern matches a string
+        return false;
+    }
+}
