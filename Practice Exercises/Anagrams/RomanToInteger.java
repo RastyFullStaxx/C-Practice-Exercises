0bea473af8aa70ca3cb451072cc68d56476e8afd
Rasty Espartero <gemrasty@gmail.com>
2024-01-04 19:54:29 +0800

    Create RomanToInteger.java

---
+++ Practice Exercises/Anagrams/RomanToInteger.java
@@ -0,0 +1,14 @@
+public class RomanToInteger {
+    public static void main(String[] args) {
+        String romanNumeral = "III";
+
+        System.out.println("Roman Numeral: " + romanNumeral);
+        int integerEquivalent = romanToInt(romanNumeral);
+        System.out.println("Integer Equivalent: " + integerEquivalent);
+    }
+
+    public static int romanToInt(String s) {
+        // Implement a method to convert a Roman numeral to an integer
+        return 0;
+    }
+}
