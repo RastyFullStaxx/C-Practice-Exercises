04cea3f017aa6c3b0d776a0158bf8e67eb4f5d8e
Rasty Espartero <gemrasty@gmail.com>
2024-01-04 19:55:00 +0800

    Create AddBinaryStrings.java

---
+++ Practice Exercises/Anagrams/AddBinaryStrings.java
@@ -0,0 +1,16 @@
+public class AddBinaryStrings {
+    public static void main(String[] args) {
+        String a = "1010";
+        String b = "1011";
+
+        System.out.println("Binary String a: " + a);
+        System.out.println("Binary String b: " + b);
+        String sumBinary = addBinary(a, b);
+        System.out.println("Sum of Binary Strings: " + sumBinary);
+    }
+
+    public static String addBinary(String a, String b) {
+        // Implement a method to add two binary strings and return their sum
+        return null;
+    }
+}
