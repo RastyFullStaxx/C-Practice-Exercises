b5ea50e3f194a4ab1d9116bf7b87b6845b07a15d
Rasty Espartero <gemrasty@gmail.com>
2024-01-04 19:54:10 +0800

    Create PalindromicSubstrings.java

---
+++ Practice Exercises/Anagrams/PalindromicSubstrings.java
@@ -0,0 +1,14 @@
+public class PalindromicSubstrings {
+    public static void main(String[] args) {
+        String s = "abc";
+
+        System.out.println("String s: " + s);
+        int palindromicSubstrings = countPalindromicSubstrings(s);
+        System.out.println("Number of Palindromic Substrings: " + palindromicSubstrings);
+    }
+
+    public static int countPalindromicSubstrings(String s) {
+        // Implement a method to count the number of palindromic substrings in a given string
+        return 0;
+    }
+}
