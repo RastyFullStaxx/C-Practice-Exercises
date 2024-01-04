0eec4f7c90c04612a7d6deb2b5e4374c826d3039
Rasty Espartero <gemrasty@gmail.com>
2024-01-04 19:53:36 +0800

    Create IsomorphicStrings.java

---
+++ Practice Exercises/Anagrams/IsomorphicStrings.java
@@ -0,0 +1,16 @@
+public class IsomorphicStrings {
+    public static void main(String[] args) {
+        String s = "egg";
+        String t = "add";
+
+        System.out.println("String s: " + s);
+        System.out.println("String t: " + t);
+        boolean isIsomorphic = isIsomorphicStrings(s, t);
+        System.out.println("Are Strings Isomorphic: " + isIsomorphic);
+    }
+
+    public static boolean isIsomorphicStrings(String s, String t) {
+        // Implement a method to check if two strings are isomorphic
+        return false;
+    }
+}
