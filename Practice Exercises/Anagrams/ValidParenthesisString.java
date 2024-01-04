5edd8bf7bd495c4902d464db7d94afc895533524
Rasty Espartero <gemrasty@gmail.com>
2024-01-04 19:54:00 +0800

    Create ValidParenthesisString.java

---
+++ Practice Exercises/Anagrams/ValidParenthesisString.java
@@ -0,0 +1,14 @@
+public class ValidParenthesisString {
+    public static void main(String[] args) {
+        String s = "(*))";
+
+        System.out.println("String s: " + s);
+        boolean isValid = checkValidString(s);
+        System.out.println("Is Valid Parenthesis String: " + isValid);
+    }
+
+    public static boolean checkValidString(String s) {
+        // Implement a method to check if the given string is a valid parenthesis string
+        return false;
+    }
+}
