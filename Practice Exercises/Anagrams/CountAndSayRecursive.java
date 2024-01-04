00c0b2f534462e3e3183f3b60ad6d07d43cc27fe
Rasty Espartero <gemrasty@gmail.com>
2024-01-04 19:54:49 +0800

    Create CountAndSayRecursive.java

---
+++ Practice Exercises/Anagrams/CountAndSayRecursive.java
@@ -0,0 +1,12 @@
+public class CountAndSayRecursive {
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println("Count and Say for n=" + n + ": " + countAndSayRecursive(n));
+    }
+
+    public static String countAndSayRecursive(int n) {
+        // Implement a recursive method to generate the nth count-and-say sequence
+        return null;
+    }
+}
