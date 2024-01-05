71b927ec7c96552c8dd04de348193bfebcdd03d4
Rasty Espartero <gemrasty@gmail.com>
2024-01-05 23:36:23 +0800

    Create ContainerWithMostWater.java

---
+++ Practice Exercises/Anagrams/ContainerWithMostWater.java
@@ -0,0 +1,14 @@
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+        int[] heights = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+        System.out.println("Heights Array: " + Arrays.toString(heights));
+        int maxArea = maxAreaContainer(heights);
+        System.out.println("Maximum Area of Water Container: " + maxArea);
+    }
+
+    public static int maxAreaContainer(int[] heights) {
+        // Implement a method to find the maximum area of water that can be held by two lines in the array
+        return 0;
+    }
+}
