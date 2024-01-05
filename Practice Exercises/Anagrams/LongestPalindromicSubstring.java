57c1d7a2880d56a84748a188cfc7a530f4abc458
Rasty Espartero <gemrasty@gmail.com>
2024-01-05 23:37:01 +0800

    Create LongestPalindromicSubstring.java

---
+++ Practice Exercises/Anagrams/LongestPalindromicSubstring.java
@@ -0,0 +1,14 @@
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        String s = "babad";
+
+        System.out.println("String s: " + s);
+        String longestPalindrome = longestPalindromeSubstring(s);
+        System.out.println("Longest Palindromic Substring: " + longestPalindrome);
+    }
+
+    public static String longestPalindromeSubstring(String s) {
+        // Implement a method to find the longest palindromic substring in a given string
+        return null;
+    }
+}
