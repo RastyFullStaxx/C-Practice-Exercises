967d27f1a55100730cd1b96756419b52f0c6ba10
Rasty Espartero <gemrasty@gmail.com>
2024-01-05 23:36:42 +0800

    Create ZigZagConversion.java

---
+++ Practice Exercises/Anagrams/ZigZagConversion.java
@@ -0,0 +1,16 @@
+public class ZigZagConversion {
+    public static void main(String[] args) {
+        String s = "PAYPALISHIRING";
+        int numRows = 3;
+
+        System.out.println("String s: " + s);
+        System.out.println("Number of Rows: " + numRows);
+        String zigzagConversion = convertZigZag(s, numRows);
+        System.out.println("ZigZag Conversion: " + zigzagConversion);
+    }
+
+    public static String convertZigZag(String s, int numRows) {
+        // Implement a method to perform ZigZag conversion on a given string
+        return null;
+    }
+}
