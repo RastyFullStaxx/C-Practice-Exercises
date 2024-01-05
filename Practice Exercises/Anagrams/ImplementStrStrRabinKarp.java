187c090a5f37161be9e881792ebe52f5ab86f33e
Rasty Espartero <gemrasty@gmail.com>
2024-01-05 23:36:14 +0800

    Create ImplementStrStrRabinKarp.java

---
+++ Practice Exercises/Anagrams/ImplementStrStrRabinKarp.java
@@ -0,0 +1,16 @@
+public class ImplementStrStrRabinKarp {
+    public static void main(String[] args) {
+        String haystack = "hello";
+        String needle = "ll";
+
+        System.out.println("Haystack: " + haystack);
+        System.out.println("Needle: " + needle);
+        int index = strStrRabinKarp(haystack, needle);
+        System.out.println("Index of Needle in Haystack: " + index);
+    }
+
+    public static int strStrRabinKarp(String haystack, String needle) {
+        // Implement strStr() using the Rabin-Karp algorithm
+        return -1;
+    }
+}
