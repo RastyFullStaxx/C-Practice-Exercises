df912137bf0ac823be26b7f009ba78e0407cb925
Rasty Espartero <gemrasty@gmail.com>
2024-01-05 23:36:04 +0800

    Create ImplementStrStrBruteForce.java

---
+++ Practice Exercises/Anagrams/ImplementStrStrBruteForce.java
@@ -0,0 +1,16 @@
+public class ImplementStrStrBruteForce {
+    public static void main(String[] args) {
+        String haystack = "hello";
+        String needle = "ll";
+
+        System.out.println("Haystack: " + haystack);
+        System.out.println("Needle: " + needle);
+        int index = strStrBruteForce(haystack, needle);
+        System.out.println("Index of Needle in Haystack: " + index);
+    }
+
+    public static int strStrBruteForce(String haystack, String needle) {
+        // Implement strStr() using the Brute Force approach
+        return -1;
+    }
+}
