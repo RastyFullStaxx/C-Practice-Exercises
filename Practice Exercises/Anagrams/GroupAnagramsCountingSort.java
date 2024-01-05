d47a6a5e6f0db6d82e1433ecc19b642f921486fc
Rasty Espartero <gemrasty@gmail.com>
2024-01-05 23:36:51 +0800

    Create GroupAnagramsCountingSort.java

---
+++ Practice Exercises/Anagrams/GroupAnagramsCountingSort.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class GroupAnagramsCountingSort {
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+        System.out.println("Array of Strings: " + Arrays.toString(strs));
+        List<List<String>> groupedAnagrams = groupAnagramsCountingSort(strs);
+        System.out.println("Grouped Anagrams: " + groupedAnagrams);
+    }
+
+    public static List<List<String>> groupAnagramsCountingSort(String[] strs) {
+        // Implement a method to group anagrams together in an array of strings using Counting Sort
+        return null;
+    }
+}
