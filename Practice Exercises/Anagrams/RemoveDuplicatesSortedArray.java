016e6de5329fc49cbfe1153c475284374398738f
Rasty Espartero <gemrasty@gmail.com>
2024-01-05 23:35:52 +0800

    Create RemoveDuplicatesSortedArray.java

---
+++ Practice Exercises/Anagrams/RemoveDuplicatesSortedArray.java
@@ -0,0 +1,14 @@
+public class RemoveDuplicatesSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 2, 3, 4, 4, 5};
+
+        System.out.println("Array of Numbers: " + Arrays.toString(nums));
+        int uniqueCount = removeDuplicates(nums);
+        System.out.println("After Removing Duplicates: " + Arrays.toString(nums) + " (Unique Count: " + uniqueCount + ")");
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        // Implement a method to remove duplicates from a sorted array in-place
+        return 0;
+    }
+}
