da9be69698d5c033b22e0ba16a4fabc35a5b28c9
Rasty Espartero <gemrasty@gmail.com>
2024-01-06 23:51:39 +0800

    Create PalindromePartitioning.java

---
+++ Practice Exercises/Anagrams/PalindromePartitioning.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class PalindromePartitioning {
+    public static void main(String[] args) {
+        String s = "aab";
+
+        System.out.println("String s: " + s);
+        List<List<String>> partitions = partitionPalindrome(s);
+        System.out.println("Palindrome Partitions: " + partitions);
+    }
+
+    public static List<List<String>> partitionPalindrome(String s) {
+        // Implement a method to partition the string into all possible palindrome substrings
+        return null;
+    }
+}
