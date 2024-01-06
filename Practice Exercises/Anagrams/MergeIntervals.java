3bdf55d7a4f720c544f0a57ff2c85ef9285ba7c3
Rasty Espartero <gemrasty@gmail.com>
2024-01-06 23:52:10 +0800

    Create MergeIntervals.java

---
+++ Practice Exercises/Anagrams/MergeIntervals.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+public class MergeIntervals {
+    public static void main(String[] args) {
+        int[][] intervals = {
+                {1, 3},
+                {2, 6},
+                {8, 10},
+                {15, 18}
+        };
+
+        System.out.println("Intervals:");
+        printIntervals(intervals);
+        int[][] mergedIntervals = mergeIntervals(intervals);
+        System.out.println("Merged Intervals:");
+        printIntervals(mergedIntervals);
+    }
+
+    public static int[][] mergeIntervals(int[][] intervals) {
+        // Implement a method to merge overlapping intervals
+        return null;
+    }
+
+    private static void printIntervals(int[][] intervals) {
+        for (int[] interval : intervals) {
+            System.out.println(Arrays.toString(interval));
+        }
+    }
+}
