4cdf5b993340e86e2e1e4e2b31c0795392668e23
Rasty Espartero <gemrasty@gmail.com>
2024-01-06 23:52:19 +0800

    Create LargestRectangleInHistogram.java

---
+++ Practice Exercises/Anagrams/LargestRectangleInHistogram.java
@@ -0,0 +1,14 @@
+public class LargestRectangleInHistogram {
+    public static void main(String[] args) {
+        int[] heights = {2, 1, 5, 6, 2, 3};
+
+        System.out.println("Heights Array: " + Arrays.toString(heights));
+        int maxRectangleArea = largestRectangleArea(heights);
+        System.out.println("Largest Rectangle Area: " + maxRectangleArea);
+    }
+
+    public static int largestRectangleArea(int[] heights) {
+        // Implement a method to find the largest rectangle area in a histogram represented by heights
+        return 0;
+    }
+}
