963ceda6bf7659a1318f8c9456340ab1677ac903
Rasty Espartero <gemrasty@gmail.com>
2024-01-06 23:52:00 +0800

    Create SearchInRotatedSortedArray.java

---
+++ Practice Exercises/Anagrams/SearchInRotatedSortedArray.java
@@ -0,0 +1,16 @@
+public class SearchInRotatedSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+
+        System.out.println("Array of Numbers: " + Arrays.toString(nums));
+        System.out.println("Target: " + target);
+        int index = searchInRotatedSortedArray(nums, target);
+        System.out.println("Index of Target: " + index);
+    }
+
+    public static int searchInRotatedSortedArray(int[] nums, int target) {
+        // Implement a method to search for the target in a rotated sorted array
+        return -1;
+    }
+}
