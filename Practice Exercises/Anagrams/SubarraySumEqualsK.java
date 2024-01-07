c2790db1b7878bc9c575ea965cacb204fc4a1d8a
Rasty Espartero <gemrasty@gmail.com>
2024-01-07 22:09:30 +0800

    Create SubarraySumEqualsK.java

---
+++ Practice Exercises/Anagrams/SubarraySumEqualsK.java
@@ -0,0 +1,15 @@
+public class SubarraySumEqualsK {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5};
+        int k = 7;
+
+        System.out.println("Array of Numbers: " + Arrays.toString(nums));
+        int countSubarrays = subarraySumEqualsK(nums, k);
+        System.out.println("Count of Subarrays with Sum K: " + countSubarrays);
+    }
+
+    public static int subarraySumEqualsK(int[] nums, int k) {
+        // Implement a method to count the number of subarrays whose sum equals k
+        return 0;
+    }
+}
