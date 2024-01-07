db988dc11829d4b1e4b4ad05d0877f91d9c27cd6
Rasty Espartero <gemrasty@gmail.com>
2024-01-07 22:08:42 +0800

    Create ProductExceptSelf.java

---
+++ Practice Exercises/Anagrams/ProductExceptSelf.java
@@ -0,0 +1,14 @@
+public class ProductExceptSelf {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+
+        System.out.println("Array of Numbers: " + Arrays.toString(nums));
+        int[] products = productExceptSelf(nums);
+        System.out.println("Product of Array Except Self: " + Arrays.toString(products));
+    }
+
+    public static int[] productExceptSelf(int[] nums) {
+        // Implement a method to calculate the product of array elements except self
+        return null;
+    }
+}
