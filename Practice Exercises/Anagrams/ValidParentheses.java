6fb7c4dc1039961d380a165eec0d5435d661b954
Rasty Espartero <gemrasty@gmail.com>
2024-01-07 22:09:18 +0800

    Create ValidParentheses.java

---
+++ Practice Exercises/Anagrams/ValidParentheses.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String s = "{[()]}";
+
+        System.out.println("String s: " + s);
+        boolean isValid = isValidParentheses(s);
+        System.out.println("Is Valid Parentheses: " + isValid);
+    }
+
+    public static boolean isValidParentheses(String s) {
+        // Implement a method to check if the given string has valid parentheses
+        return false;
+    }
+}
