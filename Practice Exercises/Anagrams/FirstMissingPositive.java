af70ed0cf385a6703083af008812a45f40ae49af
Rasty Espartero <gemrasty@gmail.com>
2024-01-07 22:10:08 +0800

    Create FirstMissingPositive.java

---
+++ Practice Exercises/Anagrams/FirstMissingPositive.java
@@ -0,0 +1,14 @@
+public class FirstMissingPositive {
+    public static void main(String[] args) {
+        int[] nums = {3, 4, -1, 1};
+
+        System.out.println("Array of Numbers: " + Arrays.toString(nums));
+        int firstMissingPositive = firstMissingPositive(nums);
+        System.out.println("First Missing Positive: " + firstMissingPositive);
+    }
+
+    public static int firstMissingPositive(int[] nums) {
+        // Implement a method to find the first missing positive integer in the array
+        return 0;
+    }
+}
