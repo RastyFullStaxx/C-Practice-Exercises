9580eeb4104ef11f0059f4d8544074d86a60f208
Rasty Espartero <gemrasty@gmail.com>
2024-01-07 22:08:54 +0800

    Create CountAndSayIterative.java

---
+++ Practice Exercises/Anagrams/CountAndSayIterative.java
@@ -0,0 +1,12 @@
+public class CountAndSayIterative {
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println("Count and Say for n=" + n + ": " + countAndSayIterative(n));
+    }
+
+    public static String countAndSayIterative(int n) {
+        // Implement an iterative method to generate the nth count-and-say sequence
+        return null;
+    }
+}
