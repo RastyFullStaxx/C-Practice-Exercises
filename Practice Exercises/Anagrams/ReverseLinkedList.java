ac2cf301e35fdb1b24fa92ec9fc461fed08a512d
Rasty Espartero <gemrasty@gmail.com>
2024-01-07 22:09:07 +0800

    Create ReverseLinkedList.java

---
+++ Practice Exercises/Anagrams/ReverseLinkedList.java
@@ -0,0 +1,38 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original Linked List:");
+        printLinkedList(head);
+        ListNode reversedList = reverseLinkedList(head);
+        System.out.println("Reversed Linked List:");
+        printLinkedList(reversedList);
+    }
+
+    public static ListNode reverseLinkedList(ListNode head) {
+        // Implement a method to reverse a linked list
+        return null;
+    }
+
+    private static void printLinkedList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
