f58b4e5b68c054764e3c3fd15b7e94ad9857ab92
Rasty Espartero <gemrasty@gmail.com>
2024-01-07 22:10:31 +0800

    Create GenerateParentheses.java

---
+++ Practice Exercises/Anagrams/GenerateParentheses.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class GenerateParentheses {
+    public static void main(String[] args) {
+        int n = 3;
+
+        System.out.println("Generate Parentheses for n=" + n + ": " + generateParentheses(n));
+    }
+
+    public static List<String> generateParentheses(int n) {
+        // Implement a method to generate all valid combinations of n pairs of parentheses
+        return null;
+    }
+}
