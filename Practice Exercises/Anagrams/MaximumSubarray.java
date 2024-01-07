851cdff519717366f05b20270ee15e7c0e348bca
Rasty Espartero <gemrasty@gmail.com>
2024-01-07 22:09:57 +0800

    Create MaximumSubarray.java

---
+++ Practice Exercises/Anagrams/MaximumSubarray.java
@@ -0,0 +1,14 @@
+public class MaximumSubarray {
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        System.out.println("Array of Numbers: " + Arrays.toString(nums));
+        int maxSubarraySum = maxSubarray(nums);
+        System.out.println("Maximum Subarray Sum: " + maxSubarraySum);
+    }
+
+    public static int maxSubarray(int[] nums) {
+        // Implement a method to find the maximum sum of a contiguous subarray
+        return 0;
+    }
+}
