8b9aaac343d962244fcad7b83ac4ad25e3089bc9
Rasty Espartero <gemrasty@gmail.com>
2024-01-08 18:57:07 +0800

    Create SortColors.java

---
+++ Practice Exercises/Anagrams/SortColors.java
@@ -0,0 +1,13 @@
+public class SortColors {
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+
+        System.out.println("Array of Colors: " + Arrays.toString(nums));
+        sortColors(nums);
+        System.out.println("Sorted Colors: " + Arrays.toString(nums));
+    }
+
+    public static void sortColors(int[] nums) {
+        // Implement a method to sort an array of colors represented by 0, 1, and 2
+    }
+}
