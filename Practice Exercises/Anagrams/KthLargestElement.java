4f7dff2824fb125670f2f6b454992e0457d1a5c8
Rasty Espartero <gemrasty@gmail.com>
2024-01-08 18:56:39 +0800

    Create KthLargestElement.java

---
+++ Practice Exercises/Anagrams/KthLargestElement.java
@@ -0,0 +1,15 @@
+public class KthLargestElement {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+
+        System.out.println("Array of Numbers: " + Arrays.toString(nums));
+        int kthLargest = findKthLargest(nums, k);
+        System.out.println("Kth Largest Element: " + kthLargest);
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+        // Implement a method to find the kth largest element in an array
+        return 0;
+    }
+}
