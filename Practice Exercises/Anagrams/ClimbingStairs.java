c5c0f920c1dfeb0b8fe9ffc8e78a37774a5e434a
Rasty Espartero <gemrasty@gmail.com>
2024-01-08 18:58:24 +0800

    Create ClimbingStairs.java

---
+++ Practice Exercises/Anagrams/ClimbingStairs.java
@@ -0,0 +1,14 @@
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        int n = 5;
+
+        System.out.println("Number of Stairs: " + n);
+        int waysToClimb = climbStairs(n);
+        System.out.println("Number of Ways to Climb Stairs: " + waysToClimb);
+    }
+
+    public static int climbStairs(int n) {
+        // Implement a method to find the number of distinct ways to climb to the top of the stairs
+        return 0;
+    }
+}
