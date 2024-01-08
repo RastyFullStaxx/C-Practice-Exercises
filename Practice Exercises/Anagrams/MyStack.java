921df1146e3088bc645348a01940a5dd39ef5844
Rasty Espartero <gemrasty@gmail.com>
2024-01-08 18:56:52 +0800

    Create MyStack.java

---
+++ Practice Exercises/Anagrams/MyStack.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MyStack {
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+
+    public MyStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        // Implement a method to push an element onto the stack
+    }
+
+    public int pop() {
+        // Implement a method to pop the element from the top of the stack
+        return 0;
+    }
+
+    public int top() {
+        // Implement a method to get the top element of the stack without popping it
+        return 0;
+    }
+
+    public boolean empty() {
+        // Implement a method to check if the stack is empty
+        return false;
+    }
+}
