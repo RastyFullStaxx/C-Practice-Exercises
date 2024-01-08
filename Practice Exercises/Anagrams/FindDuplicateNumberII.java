51d32f0ff08661b0196c828bf43039eca605f760
Rasty Espartero <gemrasty@gmail.com>
2024-01-08 18:57:36 +0800

    Create FindDuplicateNumberII.java

---
+++ Practice Exercises/Anagrams/FindDuplicateNumberII.java
@@ -0,0 +1,14 @@
+public class FindDuplicateNumber {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 4, 2, 2};
+
+        System.out.println("Array of Numbers: " + Arrays.toString(nums));
+        int duplicateNumber = findDuplicate(nums);
+        System.out.println("Duplicate Number: " + duplicateNumber);
+    }
+
+    public static int findDuplicate(int[] nums) {
+        // Implement a method to find the duplicate number in an array containing n+1 integers where each integer is between 1 and n
+        return 0;
+    }
+}
