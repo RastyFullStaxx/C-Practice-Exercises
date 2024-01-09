37bc747f33d8764b5c4807fe2bb59170ff66f503
Rasty Espartero <gemrasty@gmail.com>
2024-01-09 23:56:56 +0800

    Create Subsets.java

---
+++ Practice Exercises/Anagrams/Subsets.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class Subsets {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+
+        System.out.println("Array of Numbers: " + Arrays.toString(nums));
+        List<List<Integer>> allSubsets = subsets(nums);
+        System.out.println("All Subsets: " + allSubsets);
+    }
+
+    public static List<List<Integer>> subsets(int[] nums) {
+        // Implement a method to generate all possible subsets of a set of distinct integers
+        return null;
+    }
+}
