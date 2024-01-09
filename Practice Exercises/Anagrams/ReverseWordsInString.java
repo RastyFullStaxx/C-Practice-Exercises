8b4069ad7dcf5b665636137def02610f154c235b
Rasty Espartero <gemrasty@gmail.com>
2024-01-09 23:55:40 +0800

    Create ReverseWordsInString.java

---
+++ Practice Exercises/Anagrams/ReverseWordsInString.java
@@ -0,0 +1,14 @@
+public class ReverseWordsInString {
+    public static void main(String[] args) {
+        String s = "  hello world!  ";
+
+        System.out.println("Original String: \"" + s + "\"");
+        String reversedString = reverseWords(s);
+        System.out.println("Reversed Words: \"" + reversedString + "\"");
+    }
+
+    public static String reverseWords(String s) {
+        // Implement a method to reverse the order of words in a given string
+        return null;
+    }
+}
