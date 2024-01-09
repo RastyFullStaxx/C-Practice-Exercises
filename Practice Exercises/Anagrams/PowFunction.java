c4c8b5e6c93c6c2a946abbb9f486a7c31daccabe
Rasty Espartero <gemrasty@gmail.com>
2024-01-09 23:56:27 +0800

    Create PowFunction.java

---
+++ Practice Exercises/Anagrams/PowFunction.java
@@ -0,0 +1,16 @@
+public class PowFunction {
+    public static void main(String[] args) {
+        double x = 2.0;
+        int n = 10;
+
+        System.out.println("Base x: " + x);
+        System.out.println("Exponent n: " + n);
+        double result = myPow(x, n);
+        System.out.println("Result of Pow(x, n): " + result);
+    }
+
+    public static double myPow(double x, int n) {
+        // Implement a method to calculate x raised to the power n
+        return 0.0;
+    }
+}
