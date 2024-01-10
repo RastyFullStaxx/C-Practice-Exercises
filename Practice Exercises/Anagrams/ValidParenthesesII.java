88978289f15fbe8b22d9432dc64a39d55fc5c680
Rasty Espartero <gemrasty@gmail.com>
2024-01-10 23:38:04 +0800

    Create ValidParenthesesII.java

---
+++ Practice Exercises/Anagrams/ValidParenthesesII.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String s = "{[()]}";
+
+        System.out.println("Expression: \"" + s + "\"");
+        boolean isValid = isValid(s);
+        System.out.println("Is Valid: " + isValid);
+    }
+
+    public static boolean isValid(String s) {
+        // Implement a method to check if the given expression is valid with balanced parentheses
+        return false;
+    }
+}
