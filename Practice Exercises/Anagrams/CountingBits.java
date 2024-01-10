4101886b52984ef94e857030d48b32495f2a7759
Rasty Espartero <gemrasty@gmail.com>
2024-01-10 23:38:55 +0800

    Create CountingBits.java

---
+++ Practice Exercises/Anagrams/CountingBits.java
@@ -0,0 +1,14 @@
+public class CountingBits {
+    public static void main(String[] args) {
+        int num = 5;
+
+        System.out.println("Number: " + num);
+        int[] bitsCount = countBits(num);
+        System.out.println("Count of Bits: " + Arrays.toString(bitsCount));
+    }
+
+    public static int[] countBits(int num) {
+        // Implement a method to count the number of 1 bits for each number from 0 to num
+        return null;
+    }
+}
