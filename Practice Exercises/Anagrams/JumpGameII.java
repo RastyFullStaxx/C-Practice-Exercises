e82da9530c4646d081dca33c4101bb7d7e0a6623
Rasty Espartero <gemrasty@gmail.com>
2024-01-11 23:48:55 +0800

    Create JumpGameII.java

---
+++ Practice Exercises/Anagrams/JumpGameII.java
@@ -0,0 +1,14 @@
+public class JumpGame {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 1, 4};
+
+        System.out.println("Array of Numbers: " + Arrays.toString(nums));
+        boolean canJump = canJump(nums);
+        System.out.println("Can Jump to the End: " + canJump);
+    }
+
+    public static boolean canJump(int[] nums) {
+        // Implement a method to determine if you can reach the last index by jumping from index to index
+        return false;
+    }
+}
