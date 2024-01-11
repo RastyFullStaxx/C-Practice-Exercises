c45c424a6f03119c8c6b36db339aeaa5ea7e2c70
Rasty Espartero <gemrasty@gmail.com>
2024-01-11 23:50:46 +0800

    Create UniquePathsII.java

---
+++ Practice Exercises/Anagrams/UniquePathsII.java
@@ -0,0 +1,14 @@
+public class UniquePaths {
+    public static void main(String[] args) {
+        int m = 3, n = 7;
+
+        System.out.println("Grid Dimensions: " + m + "x" + n);
+        int uniquePaths = uniquePaths(m, n);
+        System.out.println("Number of Unique Paths: " + uniquePaths);
+    }
+
+    public static int uniquePaths(int m, int n) {
+        // Implement a method to find the number of unique paths from the top-left to the bottom-right corner
+        return 0;
+    }
+}
