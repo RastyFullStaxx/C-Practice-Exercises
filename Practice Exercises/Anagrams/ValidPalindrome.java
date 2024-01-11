503e571448e87ac8225cf476d5cf5c4177ec3788
Rasty Espartero <gemrasty@gmail.com>
2024-01-11 23:49:46 +0800

    Create ValidPalindrome.java

---
+++ Practice Exercises/Anagrams/ValidPalindrome.java
@@ -0,0 +1,14 @@
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+
+        System.out.println("Original String: \"" + s + "\"");
+        boolean isPalindrome = isPalindrome(s);
+        System.out.println("Is Valid Palindrome: " + isPalindrome);
+    }
+
+    public static boolean isPalindrome(String s) {
+        // Implement a method to check if a given string is a valid palindrome
+        return false;
+    }
+}
