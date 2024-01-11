5ebf5a6996022c552ed544b02bf0ca8230f8a31a
Rasty Espartero <gemrasty@gmail.com>
2024-01-11 23:48:37 +0800

    Create GroupAnagramsII.java

---
+++ Practice Exercises/Anagrams/GroupAnagramsII.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
+        System.out.println("Array of Strings: " + Arrays.toString(strs));
+        List<List<String>> groupedAnagrams = groupAnagrams(strs);
+        System.out.println("Grouped Anagrams: " + groupedAnagrams);
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        // Implement a method to group anagrams together from the given array of strings
+        return null;
+    }
+}
