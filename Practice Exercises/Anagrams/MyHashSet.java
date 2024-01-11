a2260ee62fde52d44859746e4f69ca9d15f00bf6
Rasty Espartero <gemrasty@gmail.com>
2024-01-11 23:50:17 +0800

    Create MyHashSet.java

---
+++ Practice Exercises/Anagrams/MyHashSet.java
@@ -0,0 +1,20 @@
+public class MyHashSet {
+    private boolean[] set;
+
+    public MyHashSet() {
+        // Implement a HashSet with basic operations: add, remove, and contains
+    }
+
+    public void add(int key) {
+        // Implement a method to add a key to the hash set
+    }
+
+    public void remove(int key) {
+        // Implement a method to remove a key from the hash set
+    }
+
+    public boolean contains(int key) {
+        // Implement a method to check if the hash set contains a specific key
+        return false;
+    }
+}
