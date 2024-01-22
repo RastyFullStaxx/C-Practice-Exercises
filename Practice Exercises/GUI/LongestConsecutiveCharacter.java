e9c0ff322db9b0ab82e58cf77f706e402c5a5406
Rasty Espartero <gemrasty@gmail.com>
2024-01-22 23:39:52 +0800

    Create LongestConsecutiveCharacter.java

---
+++ Practice Exercises/GUI/LongestConsecutiveCharacter.java
@@ -0,0 +1,14 @@
+public class LongestConsecutiveCharacter {
+    public static void main(String[] args) {
+        String s = "AAABBBCCCAA";
+
+        System.out.println("String s: " + s);
+        char longestConsecutiveChar = longestConsecutiveCharacter(s);
+        System.out.println("Longest Consecutive Character: " + longestConsecutiveChar);
+    }
+
+    public static char longestConsecutiveCharacter(String s) {
+        // Implement a method to find the character with the longest consecutive occurrences in a string
+        return ' ';
+    }
+}
