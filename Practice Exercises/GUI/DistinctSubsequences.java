fbb5af2ca38448613e5d359c093ae1f5a4200e39
Rasty Espartero <gemrasty@gmail.com>
2024-01-22 23:40:12 +0800

    Create DistinctSubsequences.java

---
+++ Practice Exercises/GUI/DistinctSubsequences.java
@@ -0,0 +1,16 @@
+public class DistinctSubsequences {
+    public static void main(String[] args) {
+        String s = "rabbbit";
+        String t = "rabbit";
+
+        System.out.println("String s: " + s);
+        System.out.println("String t: " + t);
+        int distinctSubsequences = numDistinct(s, t);
+        System.out.println("Distinct Subsequences: " + distinctSubsequences);
+    }
+
+    public static int numDistinct(String s, String t) {
+        // Implement a method to count the number of distinct subsequences of t in s
+        return 0;
+    }
+}
