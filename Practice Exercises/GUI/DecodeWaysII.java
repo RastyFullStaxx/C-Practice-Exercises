41070a967753faeff150c2cbd1435c3f4cbdc4cf
Rasty Espartero <gemrasty@gmail.com>
2024-01-22 23:40:42 +0800

    Create DecodeWaysII.java

---
+++ Practice Exercises/GUI/DecodeWaysII.java
@@ -0,0 +1,14 @@
+public class DecodeWaysII {
+    public static void main(String[] args) {
+        String s = "226";
+
+        System.out.println("String s: " + s);
+        int numDecodings = numDecodings(s);
+        System.out.println("Number of Decodings: " + numDecodings);
+    }
+
+    public static int numDecodings(String s) {
+        // Implement a method to count the number of ways to decode a given string, considering '*' as a wildcard character
+        return 0;
+    }
+}
