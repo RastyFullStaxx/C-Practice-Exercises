00feef3e58692714a8b88db0b0036e9bc2ed23fa
Rasty Espartero <gemrasty@gmail.com>
2024-02-03 22:29:31 +0800

    Add files via upload

---
+++ Programming Course/GUI/AbstractButtonsPractice.java
@@ -0,0 +1,15 @@
+package AbstractButtonPractice;
+
+
+
+// Abstract Button = an abstract class that serves as the base class for a family of button-like components
+//					 include JButton, JToggleButton, JCheckBox and JRadioButton
+
+public class AbstractButtonsPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
