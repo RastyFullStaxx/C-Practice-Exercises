c98df07ab5934fdb6e7265c083d742985ddfba25
Rasty Espartero <gemrasty@gmail.com>
2024-02-08 23:45:44 +0800

    Create StringReverser.java

---
+++ Practice Exercises/Skill Integration/StringReverser.java
@@ -0,0 +1,14 @@
+public class StringReverser {
+    public static String reverse(String str) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed.append(str.charAt(i));
+        }
+        return reversed.toString();
+    }
+    
+    public static void main(String[] args) {
+        String original = "Hello, World!";
+        System.out.println("Reversed string: " + reverse(original));
+    }
+}
