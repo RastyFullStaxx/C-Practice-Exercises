96a887a041d3c79df9e1f6ca814aecd608f8b4c2
Rasty Espartero <gemrasty@gmail.com>
2024-02-09 23:35:00 +0800

    Create ArrayReverser.java

---
+++ Practice Exercises/Skill Integration/ArrayReverser.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class ArrayReverser {
+    public static void reverseArray(int[] array) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println("Original array: " + Arrays.toString(array));
+        reverseArray(array);
+        System.out.println("Reversed array: " + Arrays.toString(array));
+    }
+}
