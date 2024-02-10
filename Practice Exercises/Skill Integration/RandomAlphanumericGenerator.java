13a8f6444f48dc0c8163372c80d4ec5ef9a86387
Rasty Espartero <gemrasty@gmail.com>
2024-02-10 23:46:30 +0800

    Create RandomAlphanumericGenerator.java

---
+++ Practice Exercises/Skill Integration/RandomAlphanumericGenerator.java
@@ -0,0 +1,19 @@
+import java.security.SecureRandom;
+
+public class RandomAlphanumericGenerator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public static String generateRandomString(int length) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(CHARACTERS.charAt(random.nextInt(CHARACTERS.length())));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        int length = 8;
+        System.out.println("Random alphanumeric string: " + generateRandomString(length));
+    }
+}
